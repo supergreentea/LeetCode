0be42486e4af9467ec8ee1a2d7e751a81d633672
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-07 19:03:33 -0400

    Time: 154 ms (18.92%), Space: 14.9 MB (22.40%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,28 +1,29 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return self.quickselect(nums, 0, len(nums) - 1, k)
         
+        def quickselect(nums, l, r, k):
+            if l == r:
+                return nums[l]
+            
+            pi = partition(nums, l, r)
+            if k - 1 == pi:
+                return nums[k - 1]
+            elif k - 1 < pi:
+                return quickselect(nums, l, pi - 1, k)
+            return quickselect(nums, pi + 1, r, k)
         
-    def quickselect(self, nums, low, high, k):
-        if low == high:
-            return nums[low]
-        pi = self.partition(nums, low, high)
-        if k - 1 == pi:
-            return nums[k - 1]
-        elif k - 1 < pi:
-            return self.quickselect(nums, low, pi - 1, k)
-        return self.quickselect(nums, pi + 1, high, k)
-    
-    def partition(self, nums, low, high):
-        pivotIndex = randint(low, high)
-        pivot = nums[pivotIndex]
-        nums[pivotIndex], nums[high] = nums[high], nums[pivotIndex]
+        def partition(nums, l, r):
+            pivotIndex = random.randint(l, r) # choose random pivot
+            nums[pivotIndex], nums[r] = nums[r], nums[pivotIndex] # swap pivot with rightmost element
+            
+            i = l - 1
+            for j in range(l, r):
+                if nums[j] > nums[r]:
+                    i += 1
+                    nums[j], nums[i] = nums[i], nums[j]
+            
+            nums[r], nums[i + 1] = nums[i + 1], nums[r]
+            return i + 1
         
-        i = low - 1
-        for j in range(low, high):
-            if nums[j] > pivot:
-                i += 1
-                nums[i], nums[j] = nums[j], nums[i]
-        nums[i + 1], nums[high] = nums[high], nums[i + 1]
-        return i + 1
-    
+        return quickselect(nums, 0, len(nums) - 1, k)
+            
