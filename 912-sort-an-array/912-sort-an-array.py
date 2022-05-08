73b5c0badbc9f69ea0bdd9bfdbba43246f7adaeb
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-08 16:53:48 -0400

    Time: 777 ms (60.91%), Space: 21.3 MB (69.87%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -7,17 +7,16 @@ class Solution:
                 quicksort(nums, l, pi - 1)
                 quicksort(nums, pi + 1, r)
         
-        # lomuto's partition algorithm
         def partition(nums, l, r):
-            pivotIndex = random.randint(l, r)
-            nums[pivotIndex], nums[r] = nums[r], nums[pivotIndex] # place pivot at end of range
-            i = l - 1
-            for j in range(l, r):
+            p = randint(l, r)
+            nums[p], nums[r] = nums[r], nums[p]
+            i = l
+            for j in range(i, r):
                 if nums[j] < nums[r]:
+                    nums[j], nums[i] = nums[i], nums[j]
                     i += 1
-                    nums[i], nums[j] = nums[j], nums[i]
-            nums[i + 1], nums[r] = nums[r], nums[i + 1]
-            return i + 1
+            nums[i], nums[r] = nums[r], nums[i]
+            return i
         
         quicksort(nums, 0, len(nums) - 1)
         return nums
