7c0712a6eda4450f15a25339081dbab38d7dd482
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-08 17:01:41 -0400

    Time: 2023 ms (8.49%), Space: 19.7 MB (8.06%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -3,27 +3,21 @@ class Solution:
         
         def quickselect(nums, l, r, k):
             if l == r:
-                return nums[l]
-            
+                return nums[r]
             pi = partition(nums, l, r)
-            if k - 1 == pi:
-                return nums[k - 1]
+            if pi == k - 1:
+                return nums[pi]
             elif k - 1 < pi:
                 return quickselect(nums, l, pi - 1, k)
             return quickselect(nums, pi + 1, r, k)
         
         def partition(nums, l, r):
-            pivotIndex = random.randint(l, r) # choose random pivot
-            nums[pivotIndex], nums[r] = nums[r], nums[pivotIndex] # swap pivot with rightmost element
-            
             i = l
             for j in range(l, r):
                 if nums[j] > nums[r]:
                     nums[j], nums[i] = nums[i], nums[j]
                     i += 1
-            
             nums[r], nums[i] = nums[i], nums[r]
             return i
         
         return quickselect(nums, 0, len(nums) - 1, k)
-            
