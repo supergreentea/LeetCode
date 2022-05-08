73a55a92f7bd0c74103e4422a0a5ba68ba365a8d
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-08 16:18:47 -0400

    Time: 236 ms (91.55%), Space: 21.1 MB (82.21%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -5,9 +5,9 @@ class Solution:
         for i in range(len(nums)):
             res[i] = prefix
             prefix *= nums[i]
-        
         postfix = 1
         for i in range(len(nums) - 1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
         return res
+        
