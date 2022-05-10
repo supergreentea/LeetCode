ac19762cf47d605576770da915a2945844f98cf2
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-10 03:39:21 -0400

    Time: 52 ms (81.63%), Space: 14.4 MB (97.44%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,3 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        Dict = defaultdict(int)
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            Dict[s[i]] += 1
+            Dict[t[i]] -= 1
+        for count in Dict.values():
+            if count != 0:
+                return False
+        return True
