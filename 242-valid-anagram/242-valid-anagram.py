9dafd642696756f1e85d1127b4eb0fe1ace090ca
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-10 03:35:14 -0400

    Time: 57 ms (69.77%), Space: 14.5 MB (35.46%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,12 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        Count = [0] * 26
-        for i in range(len(s)):
-            Count[ord(s[i]) - ord('a')] += 1
-            Count[ord(t[i]) - ord('a')] -= 1
-        for count in Count:
-            if count != 0:
-                return False
-        return True
+        return Counter(s) == Counter(t)
