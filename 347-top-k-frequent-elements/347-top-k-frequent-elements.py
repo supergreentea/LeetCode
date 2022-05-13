f1c95738e100ff95e93d1dcf670402e65918a5c7
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-13 02:54:44 -0400

    Time: 116 ms (71.88%), Space: 19.7 MB (5.04%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,27 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
+        buckets = [[] for _ in range(len(nums) + 1)]
         count = Counter(nums)
-        
-        def quickselect(nums, l, r, k) :
-            if l == r:
-                return nums[l]
-            pi = partition(nums, l, r)
-            if k - 1 < pi:
-                return quickselect(nums, l, pi - 1, k)
-            elif k - 1 > pi:
-                return quickselect(nums, pi + 1, r, k)
-            return nums[k - 1]
-        
-        def partition(nums, l, r):
-            i = l
-            for j in range(l, r):
-                if count[nums[j]] > count[nums[r]]:
-                    nums[j], nums[i] = nums[i], nums[j]
-                    i += 1
-            nums[r], nums[i] = nums[i], nums[r]
-            return i
-        
-        unique = list(count.keys())
-        quickselect(unique, 0, len(unique) - 1, k)
-        return unique[:k]
+        res = []
+        for n, c in count.items():
+            buckets[c].append(n)
+        for i in range(len(nums), -1, - 1):
+            for n in buckets[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+        return res
