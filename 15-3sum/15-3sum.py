60cf73000e77b7c1dbf11267fb6775a01648ab9e
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-13 02:39:43 -0400

    Time: 830 ms (77.83%), Space: 18.1 MB (73.01%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
-        for i in range(len(nums) - 2):
+        res = []
+        for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             l, r = i + 1, len(nums) - 1
@@ -18,5 +18,4 @@ class Solution:
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
         return res
-                
-            
+                    
