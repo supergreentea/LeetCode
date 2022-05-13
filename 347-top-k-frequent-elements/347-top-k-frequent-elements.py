825f382be517a61aa25b9df32fb130b62533b1ac
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-13 02:50:04 -0400

    Time: 139 ms (49.05%), Space: 18.8 MB (25.37%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,13 +1,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        buckets = [[] for i in range(len(nums) + 1)]
-        res = []
-        Count = Counter(nums)
-        for num, count in Count.items():
-            buckets[count].append(num)
-        for i in range(len(nums), -1, -1):
-            for num in buckets[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
-        return []
+        
+        count = Counter(nums)
+        
+        def quickselect(nums, l, r, k) :
+            if l == r:
+                return nums[l]
+            pi = partition(nums, l, r)
+            if k - 1 < pi:
+                return quickselect(nums, l, pi - 1, k)
+            elif k - 1 > pi:
+                return quickselect(nums, pi + 1, r, k)
+            return nums[k - 1]
+        
+        def partition(nums, l, r):
+            i = l
+            for j in range(l, r):
+                if count[nums[j]] > count[nums[r]]:
+                    nums[j], nums[i] = nums[i], nums[j]
+                    i += 1
+            nums[r], nums[i] = nums[i], nums[r]
+            return i
+        
+        unique = list(count.keys())
+        quickselect(unique, 0, len(unique) - 1, k)
+        return unique[:k]
