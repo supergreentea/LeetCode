f3a84f1fa7e0c3109036a3d920f71e17e1503a8a
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-13 03:37:16 -0400

    Time: 897 ms (43.25%), Space: 21.2 MB (86.19%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -11,11 +11,11 @@ class Solution:
             p = randint(l, r)
             nums[p], nums[r] = nums[r], nums[p]
             i = l
-            for j in range(i, r):
+            for j in range(l, r):
                 if nums[j] < nums[r]:
-                    nums[j], nums[i] = nums[i], nums[j]
+                    nums[i], nums[j] = nums[j], nums[i]
                     i += 1
-            nums[i], nums[r] = nums[r], nums[i]
+            nums[r], nums[i] = nums[i], nums[r]
             return i
         
         quicksort(nums, 0, len(nums) - 1)
