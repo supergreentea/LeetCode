6ce21bc32276f82e0ca502242636b12c72ab989c
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-15 03:11:57 -0400

    Time: 604 ms (5.03%), Space: 13.9 MB (57.78%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        maxlen = 0
+        
+        Dict = defaultdict(int)
+        
+        def highestCount(Dict):
+            res = 0
+            for count in Dict.values():
+                res = max(count, res)
+            return res
+        
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            Dict[c] += 1
+            while r - l + 1 - highestCount(Dict) > k:
+                Dict[s[l]] -= 1
+                l += 1
+            maxlen = max(maxlen, r - l + 1)
+        return maxlen
