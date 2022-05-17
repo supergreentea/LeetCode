e5bdb4e0dd232d2fae51002a5d1303556fde49d3
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-17 01:28:11 -0400

    Time: 42 ms (46.36%), Space: 14 MB (12.46%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,7 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        i, j = 0 , 1
-        for k in range(n):
+        i, j = 0, 1
+        for _ in range(n):
             t = i + j
             i = j
             j = t
