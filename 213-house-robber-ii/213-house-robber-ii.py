28ab5b15a9f00cd507e792bb9ec36783dc795160
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-18 02:27:09 -0400

    Time: 66 ms (8.55%), Space: 14 MB (6.88%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -3,18 +3,17 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         
-        dp1 = [None for _ in range(len(nums) + 1)] # consider houses from 0 to n - 2
-        dp2 = [None for _ in range(len(nums) + 1)] # consider houses from 1 to n - 1
-        
         N = len(nums)
+        dp1, dp2 = [None for _ in range(N + 1)], [None for _ in range(N + 1)]
         
+        # 0 to N - 2 for dp1
         dp1[N - 1], dp1[N - 2] = 0, nums[N - 2]
-        dp2[N], dp2[N - 1] = 0, nums[N - 1] 
-        
         for i in range(N - 3, -1, -1):
-            dp1[i] = max(nums[i] + dp1[i + 2], dp1[i + 1])
+            dp1[i] = max(dp1[i + 1], nums[i] + dp1[i + 2])
         
+        # 1 to N - 1 for dp2
+        dp2[N], dp2[N - 1] = 0, nums[N - 1]
         for i in range(N - 2, 0, -1):
-            dp2[i] = max(nums[i] +dp2[i + 2], dp2[i + 1])
+            dp2[i] = max(dp2[i + 1], nums[i] + dp2[i + 2])
         
         return max(dp1[0], dp2[1])
