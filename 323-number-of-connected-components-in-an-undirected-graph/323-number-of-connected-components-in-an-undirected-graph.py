2046fd0efb5bdc436eef2426c3ccbc74a519f1cb
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-18 19:22:19 -0400

    Time: 116 ms (68.46%), Space: 15.4 MB (67.81%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -2,11 +2,10 @@ class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         par = [i for i in range(n)]
         rank = [1] * n
-        res = n
         
         def find(n1):
             while n1 != par[n1]:
-                par[n1] = par[par[n1]]
+                par[n1] = par[par[n1]] # for path compression
                 n1 = par[n1]
             return n1
         
@@ -14,15 +13,17 @@ class Solution:
             p1, p2 = find(n1), find(n2)
             if p1 == p2:
                 return 0
-            if rank[p1] > rank[p2]:
-                par[p2] = p1
-                rank[p1] += rank[p2]
-            else:
+            if rank[p1] < rank[p2]:
                 par[p1] = p2
                 rank[p2] += rank[p1]
+            else:
+                par[p2] = p1
+                rank[p1] += rank[p2]
             return 1
         
+        res = n
         for n1, n2 in edges:
             res -= union(n1, n2)
         
         return res
+                
