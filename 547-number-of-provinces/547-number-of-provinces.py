344bd6285e9a54066a711b4f9dfc2d28eed3044b
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-18 04:24:39 -0400

    Time: 199 ms (86.26%), Space: 14.2 MB (96.66%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -4,6 +4,9 @@ class Solution:
         par = [i for i in range(n)]
         rank = [1] * n
         
+        res = n
+        
+        
         def find(n1):
             while n1 != par[n1]:
                 par[n1] = par[par[n1]]
@@ -14,20 +17,17 @@ class Solution:
             p1, p2 = find(n1), find(n2)
             if p1 == p2:
                 return 0
-            
             if rank[p1] > rank[p2]:
                 par[p2] = p1
-                rank[p1] += rank[p1]
+                rank[p1] += rank[p2]
             else:
                 par[p1] = p2
                 rank[p2] += rank[p1]
-            
             return 1
         
-        res = n
         for i in range(n):
             for j in range(n):
                 if isConnected[i][j] == 1:
                     res -= union(i, j)
+        
         return res
-                    
