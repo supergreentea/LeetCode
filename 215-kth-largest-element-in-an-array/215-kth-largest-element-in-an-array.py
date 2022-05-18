64e4938365eca057f3f29a53b4da8e270ad61393
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-18 02:02:25 -0400

    Time: 105 ms (40.62%), Space: 14.9 MB (42.68%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,31 +1,27 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
+        
         def quickselect(nums, l, r, k):
             if l == r:
                 return nums[l]
             
             pi = partition(nums, l, r)
-            
             if k - 1 < pi:
                 return quickselect(nums, l, pi - 1, k)
             if k - 1 > pi:
                 return quickselect(nums, pi + 1, r, k)
             return nums[k - 1]
-        
+            
         def partition(nums, l, r):
             p = randint(l, r)
             nums[p], nums[r] = nums[r], nums[p]
-            
             i = l
             for j in range(l, r):
                 if nums[j] > nums[r]:
-                    nums[j], nums[i] = nums[i], nums[j]
+                    nums[i], nums[j] = nums[j], nums[i]
                     i += 1
-            
             nums[r], nums[i] = nums[i], nums[r]
             return i
         
         return quickselect(nums, 0, len(nums) - 1, k)
-            
-            
