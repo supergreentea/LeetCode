8b8afe38e902a21713e52a83c5e425e7747bac21
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-18 02:09:02 -0400

    Time: 73 ms (12.41%), Space: 14.4 MB (78.14%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -15,8 +15,8 @@ class Solution:
                 return oldToNew[node]
             copy = Node(node.val)
             oldToNew[node] = copy
-            for n in node.neighbors:
-                copy.neighbors.append(clone(n))
+            if node.neighbors:
+                copy.neighbors = [clone(n) for n in node.neighbors]
             return copy
         
         return clone(node) if node else None
