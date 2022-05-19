c020b30c4090b70d10318a8c02de4eaf4685cfe0
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-19 04:11:30 -0400

    Time: 117 ms (89.82%), Space: 13.9 MB (76.23%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,7 +1,6 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0
-        
         def expand(Left, Right):
             l, r = Left, Right
             nonlocal res
@@ -9,9 +8,7 @@ class Solution:
                 res += 1
                 l -= 1
                 r += 1
-        
         for i in range(len(s)):
             expand(i, i)
             expand(i, i + 1)
-        
         return res
