8704a7eca0bbc45bae1e41d54dea6dcd24cbd1af
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-19 02:31:16 -0400

    Time: 45 ms (47.17%), Space: 13.9 MB (25.27%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,19 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
         
-        N = len(nums)
-        dp1, dp2 = [None for _ in range(N + 1)], [None for _ in range(N + 1)]
-        
-        # 0 to N - 2 for dp1
-        dp1[N - 1], dp1[N - 2] = 0, nums[N - 2]
-        for i in range(N - 3, -1, -1):
-            dp1[i] = max(dp1[i + 1], nums[i] + dp1[i + 2])
-        
-        # 1 to N - 1 for dp2
-        dp2[N], dp2[N - 1] = 0, nums[N - 1]
-        for i in range(N - 2, 0, -1):
-            dp2[i] = max(dp2[i + 1], nums[i] + dp2[i + 2])
-        
-        return max(dp1[0], dp2[1])
+    def helper(self, nums):
+        r1, r2 = 0, 0
+        for n in nums:
+            t = max(r1 + n, r2)
+            r1, r2 = r2, t
+        return r2
