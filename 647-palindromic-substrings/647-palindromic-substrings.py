a9d7bf61f68f6bc6f562748ec441922e90488387
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-19 04:10:54 -0400

    Time: 116 ms (91.45%), Space: 13.8 MB (76.23%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        
+        def expand(Left, Right):
+            l, r = Left, Right
+            nonlocal res
+            while 0 <= l and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+        
+        for i in range(len(s)):
+            expand(i, i)
+            expand(i, i + 1)
+        
+        return res
