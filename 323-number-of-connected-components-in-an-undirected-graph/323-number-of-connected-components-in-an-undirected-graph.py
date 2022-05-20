cc02b32c8a8c1db3457e997fd5fc5261149415db
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-20 03:08:53 -0400

    Time: 128 ms (57.14%), Space: 15.3 MB (87.32%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -5,7 +5,7 @@ class Solution:
         
         def find(n1):
             while n1 != par[n1]:
-                par[n1] = par[par[n1]] # for path compression
+                par[n1] = par[par[n1]]
                 n1 = par[n1]
             return n1
         
@@ -13,17 +13,15 @@ class Solution:
             p1, p2 = find(n1), find(n2)
             if p1 == p2:
                 return 0
-            if rank[p1] < rank[p2]:
-                par[p1] = p2
-                rank[p2] += rank[p1]
-            else:
+            if rank[p1] > rank[p2]:
                 par[p2] = p1
                 rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
             return 1
         
         res = n
         for n1, n2 in edges:
             res -= union(n1, n2)
-        
         return res
-                
