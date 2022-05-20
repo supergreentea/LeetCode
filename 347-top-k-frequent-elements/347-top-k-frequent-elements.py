19bcb577e6b9b87a88669802263db29345c1254d
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-20 04:15:26 -0400

    Time: 131 ms (56.17%), Space: 19.7 MB (5.22%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -3,11 +3,14 @@ class Solution:
         buckets = [[] for _ in range(len(nums) + 1)]
         count = Counter(nums)
         res = []
-        for n, c in count.items():
-            buckets[c].append(n)
-        for i in range(len(nums), -1, - 1):
+        
+        for key, value in count.items():
+            buckets[value].append(key)
+        
+        for i in range(len(nums), -1, -1):
             for n in buckets[i]:
                 res.append(n)
                 if len(res) == k:
                     return res
-        return res
+        
+        return []
