e4833030fdd97cc79a330a72ce91d13182d45e0d
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-22 04:20:38 -0400

    Time: 86 ms (16.46%), Space: 17.9 MB (86.66%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -9,11 +9,12 @@ class Solution:
         stack = []
         cur = root
         while stack or cur:
+            # go left as much as possible
             while cur:
                 stack.append(cur)
                 cur = cur.left
             k -= 1
             cur = stack.pop()
-            if k == 0:
+            if k == 0: # we visited k nodes, so current node is kth smallest
                 return cur.val
-            cur = cur.right
+            cur = cur.right # visit right subtree
