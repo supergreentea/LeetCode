f96544c2d94e87807d86b7ffb0b753c3e7d3d2a5
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-22 02:29:56 -0400

    Time: 179 ms (36.55%), Space: 17.6 MB (94.02%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -5,28 +5,31 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-    
-        class Wrapper():
-            def __init__(self, node):
-                self.node = node
-            def __lt__(self, other):
-                return self.node.val < other.node.val
-    
+        if not lists or len(lists) == 0:
+            return None
+        
         head = pointer = ListNode(0)
-        heap = []
-
-        for l in lists:
-            cur = l
-            while cur:
-                heapq.heappush(heap, Wrapper(cur))
-                cur = cur.next
-
-        while len(heap) > 0:
-            node = heapq.heappop(heap).node
-            pointer.next = node
-            pointer = pointer.next
-        pointer.next = None
-        return head.next
-    
-    
-    
+        
+        def merge(list1, list2):
+            prehead = pointer = ListNode(0)
+            while list1 and list2:
+                if list1.val < list2.val:
+                    pointer.next = list1
+                    list1 = list1.next
+                else:
+                    pointer.next = list2
+                    list2 = list2.next
+                pointer = pointer.next
+            pointer.next = list1 if list1 else list2
+            return prehead.next
+        
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                list1 = lists[i]
+                list2 = lists[i + 1] if i + 1 < len(lists) else None
+                mergedLists.append(merge(list1, list2))
+            lists = mergedLists
+        
+        return lists[0]
+            
