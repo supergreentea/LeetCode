c931f05f553d1bfdc3ec67c24f0d22004261b3c1
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-23 02:12:28 -0400

    Time: 187 ms (51.38%), Space: 14.1 MB (18.91%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -4,9 +4,8 @@ class Solution:
         l = longest = 0
         for r in range(len(s)):
             count[s[r]] += 1
-            while r - l  + 1 - max(count.values()) > k:
+            while r - l + 1 - max(count.values()) > k:
                 count[s[l]] -= 1
                 l += 1
             longest = max(longest, r - l + 1)
         return longest
-            
