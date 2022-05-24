0c1992bb31f2061beb3e3cc1350e76f0796d509f
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-23 20:01:56 -0400

    Time: 68 ms (17.49%), Space: 13.7 MB (99.03%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -6,7 +6,6 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         head = pointer = ListNode(0)
-        
         while list1 and list2:
             if list1.val < list2.val:
                 pointer.next = list1
@@ -15,6 +14,5 @@ class Solution:
                 pointer.next = list2
                 list2 = list2.next
             pointer = pointer.next
-        
         pointer.next = list1 if list1 else list2
         return head.next
