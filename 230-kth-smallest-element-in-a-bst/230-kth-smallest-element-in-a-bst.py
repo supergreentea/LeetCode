81d2490e54e8703c781122ac43c2e21b3dedaad8
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-24 03:23:43 -0400

    Time: 75 ms (26.30%), Space: 18 MB (42.21%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -11,9 +11,9 @@ class Solution:
             while cur:
                 stack.append(cur)
                 cur = cur.left
+            
             cur = stack.pop()
             k -= 1
             if k == 0:
                 return cur.val
             cur = cur.right
-                
