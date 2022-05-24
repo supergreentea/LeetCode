d1c0713566ee8a884a2064798aa243dc6d347358
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-24 00:15:48 -0400

    Time: 118 ms (75.23%), Space: 17.7 MB (74.75%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -8,10 +8,8 @@ class Solution:
         if not lists or len(lists) == 0:
             return None
         
-        head = pointer = ListNode(0)
-        
-        def merge(list1, list2):
-            prehead = pointer = ListNode(0)
+        def mergeTwoLists(list1, list2):
+            head = pointer = ListNode(0)
             while list1 and list2:
                 if list1.val < list2.val:
                     pointer.next = list1
@@ -21,15 +19,14 @@ class Solution:
                     list2 = list2.next
                 pointer = pointer.next
             pointer.next = list1 if list1 else list2
-            return prehead.next
+            return head.next
         
         while len(lists) > 1:
-            mergedLists = []
+            merged_lists = []
             for i in range(0, len(lists), 2):
                 list1 = lists[i]
                 list2 = lists[i + 1] if i + 1 < len(lists) else None
-                mergedLists.append(merge(list1, list2))
-            lists = mergedLists
+                merged_lists.append(mergeTwoLists(list1, list2))
+            lists = merged_lists
         
         return lists[0]
-            
