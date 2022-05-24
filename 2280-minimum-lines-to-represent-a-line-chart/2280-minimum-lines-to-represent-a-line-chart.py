4d5abdf888ec955634f4abb5de471d3ff4e898c9
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-24 00:33:48 -0400

    Time: 2637 ms (40.05%), Space: 59.8 MB (16.94%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.py
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.py
@@ -1,16 +1,15 @@
-from fractions import Fraction
-
 class Solution:
     def minimumLines(self, stockPrices: List[List[int]]) -> int:
-        stockPrices.sort(key=lambda x: x[0])
         if len(stockPrices) < 2:
             return 0
-        lines = 1
-        prevSlope = Fraction((stockPrices[1][1] - stockPrices[0][1]), (stockPrices[1][0] - stockPrices[0][0]))
-        for i in range(2, len(stockPrices)):
-            slope = Fraction((stockPrices[i][1] - stockPrices[i - 1][1]), (stockPrices[i][0] - stockPrices[i - 1][0]))
-            if prevSlope != slope:
-                lines += 1
-                prevSlope = slope
-        return lines
+        stockPrices.sort(key=lambda x: x[0])
+        count = 1
+        deltaX, deltaY = stockPrices[1][0] - stockPrices[0][0], stockPrices[1][1] - stockPrices[0][1]
         
+        for i in range(2, len(stockPrices)):
+            newDeltaX, newDeltaY = stockPrices[i][0] - stockPrices[i - 1][0], stockPrices[i][1] - stockPrices[i - 1][1]
+            if deltaX * newDeltaY != deltaY * newDeltaX:
+                count += 1
+            deltaX, deltaY = newDeltaX, newDeltaY
+        
+        return count
