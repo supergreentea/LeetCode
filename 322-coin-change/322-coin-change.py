afeee324ac5ea96852d267d595a8b960fdc89d66
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-25 03:24:09 -0400

    Time: 2631 ms (19.60%), Space: 14.2 MB (46.53%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -4,8 +4,8 @@ class Solution:
         # O(amount) space complexity 
         dp = [amount + 1] * (amount + 1)
         dp[0] = 0
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - c])
+        for coin in coins:
+            for a in range(coin, amount + 1):
+                dp[a] = min(dp[a], 1 + dp[a - coin])
+                
         return dp[amount] if dp[amount] != amount + 1 else -1
