18a16a22cc89618b85791b02792f1e20c0adaf0a
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-29 22:18:58 -0400

    Time: 2017 ms (79.08%), Space: 59.7 MB (28.45%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.py
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.py
@@ -1,18 +1,14 @@
 class Solution:
     def minimumLines(self, stockPrices: List[List[int]]) -> int:
+        stockPrices.sort(key = lambda x : x[0])
         if len(stockPrices) < 2:
             return 0
-        res = 1
-        stockPrices.sort(key = lambda x: x[0])
-        # a / b = c / d <= > a * d = b * c
+        if len(stockPrices) == 2:
+            return 1
         
-        delta_x, delta_y = stockPrices[1][0] - stockPrices[0][0], stockPrices[1][1] - stockPrices[0][1]
+        lines = 1
+        for i in range(1, len(stockPrices) - 1):
+            if (stockPrices[i][0] - stockPrices[i-1][0]) * (stockPrices[i+1][1] - stockPrices[i][1]) != (stockPrices[i][1] - stockPrices[i-1][1]) * (stockPrices[i+1][0] - stockPrices[i][0]):
+                lines += 1
         
-        for i in range(2, len(stockPrices)):
-            delta_x_prime, delta_y_prime = stockPrices[i][0] - stockPrices[i - 1][0], stockPrices[i][1] - stockPrices[i - 1][1]
-            if delta_x_prime * delta_y != delta_y_prime * delta_x:
-                res += 1
-                delta_x, delta_y = delta_x_prime, delta_y_prime
-        return res
-            
-        
+        return lines
