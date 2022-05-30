90c6bc10bab773053e8860f9641d543ba3a841c6
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-30 00:08:48 -0400

    Time: 46 ms (39.12%), Space: 14 MB (10.72%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
+        if not head:
             return head
         slow = fast = head
         while fast and fast.next:
