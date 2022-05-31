6b44d1ebd29ffff4d3275d6d37fdcd93174d5e62
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-31 03:14:11 -0400

    Time: 2576 ms (21.93%), Space: 14.3 MB (39.76%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,19 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = defaultdict(int)
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
         
-        def helper(remain):
-            if remain in memo:
-                return memo[remain]
-            if remain == 0:
-                return 0
-            num_coins = math.inf
+        for i in range(1, amount + 1):
             for coin in coins:
-                if remain - coin >= 0 and helper(remain - coin) != -1:
-                    num_coins = min(num_coins, helper(remain - coin) + 1)
-            num_coins = -1 if num_coins == math.inf else num_coins
-            memo[remain] = num_coins
-            return num_coins
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
         
-        return helper(amount)
-            
+        return dp[amount] if dp[amount] != amount + 1 else -1
