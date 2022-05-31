2e04701561abcd951586ce97b8a5f28babc5056b
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-05-31 03:19:45 -0400

    Time: 43 ms (76.84%), Space: 14.1 MB (49.38%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,17 +1,18 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res = []
+        subsets = []
         
-        def helper(start, subset):
-            res.append(subset.copy())
-            for i in range(start, len(nums)):
-                if i > start and nums[i] == nums[i-1]:
-                    continue
-                subset.append(nums[i])
-                helper(i + 1, subset)
-                subset.pop()
-        
-        helper(0, [])
-        return res
+        def backtrack(start, subset):
+            subsets.append(subset.copy())
             
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]: # avoid duplicates
+                    continue
+                
+                # backtrack
+                subset.append(nums[i])
+                backtrack(i + 1, subset)
+                subset.pop()
+        backtrack(0, [])
+        return subsets
