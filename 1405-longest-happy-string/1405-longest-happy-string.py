1829ae6f23e4508b210d0b7cf31ddd06ab30a5c0
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-01 03:03:23 -0400

    Time: 37 ms (72.09%), Space: 13.8 MB (91.18%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -0,0 +1,42 @@
+class Freq:
+    def __init__(self, char, count):
+        self.char = char
+        self.count = count
+        
+    def __lt__(self, other):
+        return self.count > other.count
+    
+    def __repr__(self):
+        return f'[Char: {self.char}, Count: {self.count}]'
+
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        heap = []
+        if a > 0:
+            heap.append(Freq("a", a))
+        if b > 0:
+            heap.append(Freq("b", b))
+        if c > 0:
+            heap.append(Freq("c", c))
+
+        heapq.heapify(heap)
+        res = ""
+        while heap:
+            print(heap)
+            print(res)
+            prev = cur = heapq.heappop(heap)
+            
+            if len(res) >= 1 and res[-1] == cur.char:
+                if not heap: # last element in heap forces us to break condition
+                    print("empty heap")
+                    return res
+                cur = heapq.heappop(heap) # get another element
+                heapq.heappush(heap, prev) # push prev element back to heap
+                
+            t = min(cur.count, 2) if prev == cur else 1 # always choose 1 if less frequent char is used
+            res += t * cur.char
+            cur.count = cur.count - t
+            if cur.count > 0:
+                heapq.heappush(heap, cur)
+                        
+        return res
