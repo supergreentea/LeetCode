bb5c298e3653dc0418a20987952cd9d60c8a658d
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-02 18:17:39 -0400

    Time: 185 ms (28.15%), Space: 16.1 MB (17.52%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,18 +1,25 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        
         n = len(height)
-        if n < 3:
-            return 0
-        left = [0] * n
-        right = [0] * n
-        left[0] = height[0]
-        right[n-1] = height[n-1]
-        for i in range(1, n):
-            left[i] = max(left[i-1], height[i])
-        for i in range(n-2, -1, -1):
-            right[i] = max(right[i+1], height[i])
-        res = 0
-        for i in range(1, n-1):
-            if height[i] < left[i] and height[i] < right[i]:
-                res += min(left[i], right[i]) - height[i]
-        return res
+        
+        max_left = [0] * n
+        
+        max_right = [0] * n
+        
+        max_left[0], max_right[n - 1] = height[0], height[n - 1]
+        
+        total = 0
+        
+        for i in range(1, len(height)):
+            max_left[i] = max(max_left[i - 1], height[i])
+        
+        max_right[n - 1] = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            max_right[i] = max(max_right[i + 1], height[i])
+        
+        for i in range(1, n - 1):
+            if height[i] < max_left[i] and height[i] < max_right[i]:
+                total += min(max_left[i], max_right[i]) - height[i]
+        
+        return total
