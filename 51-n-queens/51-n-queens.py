07206cd7b801e03aa27ab69a8ebc35de6ddf8fee
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-03 21:30:03 -0400

    Time: 67 ms (78.17%), Space: 14.4 MB (45.52%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -8,26 +8,27 @@ class Solution:
                     solution.append("".join(r))
                 output.append(solution)
             
-            for col in range(n):
-                # continue if queen cannot be placed
-                if (col in cols) or (row - col in diag) or (row + col) in antidiag:
+            for col in range(0, n):
+                if col in cols or row - col in diag or row + col in antidiag:
                     continue
+
                 # place queen
-                board[row][col] = "Q"
+                board[row][col] = 'Q'
                 cols.add(col)
                 diag.add(row - col)
                 antidiag.add(row + col)
                 
                 backtrack(row + 1)
                 
-                # remove queen
-                board[row][col] = "."
+                # remove queen to try placing in next column
+                board[row][col] = '.'
                 cols.remove(col)
                 diag.remove(row - col)
                 antidiag.remove(row + col)
-        
-        output = []
-        board = [["."] * n for _ in range(n)]
+            
+        board = [['.'] * n for _ in range(n)]
         cols, diag, antidiag = set(), set(), set()
+        output = []
         backtrack(0)
         return output
+                
