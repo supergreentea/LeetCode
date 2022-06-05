75993287988ad7548e5fece66bbb9c391c605bbe
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-05 02:46:51 -0400

    Time: 120 ms (29.21%), Space: 14.4 MB (47.65%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.py
@@ -11,14 +11,15 @@ class Codec:
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        res, i = [], 0
+        res = []
+        i = 0
         while i < len(s):
             j = i
-            while s[j] != '#':
+            while s[j] != "#":
                 j += 1
-            length = int(s[i : j])
-            res.append(s[j + 1 : j + 1 + length])
-            i = j + 1 + length
+            stringLength = int(s[i : j])
+            res.append(s[j + 1 : j + stringLength + 1])
+            i = j + stringLength + 1
         return res
         
 
