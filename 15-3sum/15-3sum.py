b66bca1f34d1498ee6ac752a2ee0336143d3089d
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-05 03:13:03 -0400

    Time: 1335 ms (44.81%), Space: 18.1 MB (71.81%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -3,19 +3,20 @@ class Solution:
         nums.sort()
         res = []
         
-        for i in range(len(nums)):
+        for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             l, r = i + 1, len(nums) - 1
             while l < r:
-                t = nums[i] + nums[l] + nums[r]
-                if t > 0:
-                    r -= 1
-                elif t < 0:
+                three_sum = nums[i] + nums[l] + nums[r]
+                if three_sum < 0:
                     l += 1
+                elif three_sum > 0:
+                    r -= 1
                 else:
                     res.append([nums[i], nums[l], nums[r]])
                     l += 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
+        
         return res
