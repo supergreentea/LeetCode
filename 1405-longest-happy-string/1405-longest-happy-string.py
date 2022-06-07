5bd2ca764acb50f240c1c933a02fc0364333963a
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-06 23:31:19 -0400

    Time: 57 ms (17.90%), Space: 13.8 MB (91.16%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -1,36 +1,46 @@
 class Freq:
+    
     def __init__(self, char, count):
-        self.char = char
-        self.count = count
+        self.char, self.count = char, count
         
     def __lt__(self, other):
         return self.count > other.count
-    
-    def __repr__(self):
-        return f'[Char: {self.char}, Count: {self.count}]'
 
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        freqs = [Freq("a", a), Freq("b", b), Freq("c", c)]
-        heap = list(filter(lambda x : x.count > 0, freqs))
-
+        heap = []
         heapq.heapify(heap)
+        if a > 0:
+            heapq.heappush(heap, Freq("a", a))
+        if b > 0:
+            heapq.heappush(heap, Freq("b", b))
+        if c > 0:
+            heapq.heappush(heap, Freq("c", c))
+        
         res = ""
+        
         while heap:
-            print(heap)
-            print(res)
-            prev = cur = heapq.heappop(heap)
-            
-            if len(res) >= 1 and res[-1] == cur.char:
-                if not heap: # last element in heap forces us to break condition
+            cur = heapq.heappop(heap)
+            if len(res) > 0 and res[-1] == cur.char: 
+                if not heap:
                     return res
-                cur = heapq.heappop(heap) # get another element to avoid making three or more consecutive of one character
-                heapq.heappush(heap, prev) # push prev element back to heap
-                
-            t = min(cur.count, 2) if prev == cur else 1 # always choose 1 if less frequent char is used
-            res += t * cur.char
-            cur.count = cur.count - t
-            if cur.count > 0:
-                heapq.heappush(heap, cur)
-                        
+                t = cur
+                cur = heapq.heappop(heap) # pop 2nd most frequent
+                res += cur.char
+                cur.count -= 1
+                if cur.count > 0:
+                    heapq.heappush(heap, cur)
+                heapq.heappush(heap, t)
+            else:
+                if cur.count >= 2:
+                    res += cur.char * 2
+                    cur.count -= 2
+                else:
+                    res += cur.char
+                    cur.count -= 1
+                if cur.count > 0:
+                    heapq.heappush(heap, cur)
+        
         return res
+                
+        
