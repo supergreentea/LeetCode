9b0b3e7a59ddecbcaf7f17da1a024a538d82f16c
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-08 19:09:40 -0400

    Time: 136 ms (55.00%), Space: 20.1 MB (5.43%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,31 +1,19 @@
 class Solution:
-    def topKFrequent(self, arr: List[int], k: int) -> List[int]:
-        frequencies = Counter(arr)
-        nums = list(frequencies.keys())
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        buckets = [[] for _ in range(len(nums) + 1)]
+        frequencies = Counter(nums)
+        output = []
         
-        def quickselect(l, r):
-            if l == r:
-                return nums[l]
-            pi = partition(l, r)
-            if k - 1 < pi:
-                return quickselect(l, pi - 1)
-            elif k - 1 > pi:
-                return quickselect(pi + 1, r)
-            return nums[k - 1]
+        for num, count in frequencies.items():
+            buckets[count].append(num)
         
-        def partition(l, r):
-            p = randint(l, r)
-            nums[p], nums[r] = nums[r], nums[p]
-            i = l
-            for j in range(l, r):
-                if frequencies[nums[j]] > frequencies[nums[r]]:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    i += 1
-            nums[i], nums[r] = nums[r], nums[i]
-            return i
-        
-        quickselect(0, len(nums) - 1)
-        return nums[:k]
-        
-        
+        print(buckets)
             
+        for i in range(len(nums), -1, -1):
+            for n in buckets[i]:
+                output.append(n)
+                print(len(output))
+                if len(output) == k:
+                    return output
+        
+        return output
