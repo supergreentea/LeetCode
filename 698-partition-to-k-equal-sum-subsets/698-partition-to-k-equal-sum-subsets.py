629b63149f241fcea1b9506e74878654dbf2c613
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-08 06:19:05 -0400

    Time: 1754 ms (11.62%), Space: 42.4 MB (10.61%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -4,29 +4,29 @@ class Solution:
         n = len(nums)
         if total % k != 0:
             return False
+        memo = {}
         taken = ['0'] * n
         target = total // k
-        memo = {}
         
         def backtrack(index, count, curr_sum):
-            n = len(nums)
-            taken_string = ''.join(taken)
+            taken_str = ''.join(taken)
             if count == k - 1:
                 return True
             if curr_sum > target:
                 return False
-            if taken_string in memo:
-                return memo[taken_string]
+            if taken_str in memo:
+                return memo[taken_str]
             if curr_sum == target:
-                memo[taken_string] = backtrack(0, count + 1, 0) 
-                return memo[taken_string]
+                memo[taken_str] = backtrack(0, count + 1, 0)
+                return memo[taken_str]
             for j in range(index, n):
                 if taken[j] == '0':
                     taken[j] = '1'
                     if backtrack(index + 1, count, curr_sum + nums[j]):
                         return True
                     taken[j] = '0'
-            memo[taken_string] = False
-            return memo[taken_string]
+            memo[taken_str] = False
+            return memo[taken_str]
         
         return backtrack(0, 0, 0)
+            
