cdb1fbdc715e653de1fb83987657010242e87ede
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-08 19:38:24 -0400

    Time: 146 ms (48.53%), Space: 20 MB (9.48%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,11 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
         anagrams = defaultdict(list)
         
         for s in strs:
-            count = [0] * 26
+            char_freq = [0] * 26
             for c in s:
-                count[ord(c) - ord('a')] += 1
-            anagrams[tuple(count)].append(s)
-        
+                char_freq[ord(c) - ord('a')] += 1
+            anagrams[tuple(char_freq)].append(s)
         return anagrams.values()
