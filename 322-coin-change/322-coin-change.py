d2c9d83de068aad1e688c498526d54945a5cc4eb
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-08 15:19:09 -0400

    Time: 2587 ms (22.43%), Space: 14.3 MB (47.96%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,5 +1,6 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        
         dp = [amount + 1] * (amount + 1)
         dp[0] = 0
         
@@ -9,3 +10,4 @@ class Solution:
                     dp[i] = min(dp[i], 1 + dp[i - coin])
         
         return dp[amount] if dp[amount] != amount + 1 else -1
+        
