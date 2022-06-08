c4cc1be386a671c168cbfe7c8e66d350d197e39c
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-08 05:38:23 -0400

    Time: 1610 ms (12.81%), Space: 46 MB (8.29%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -1,32 +1,32 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        if sum(nums) % k != 0:
+        total = sum(nums)
+        n = len(nums)
+        if total % k != 0:
             return False
-        nums.sort(reverse=True)
-        target = sum(nums) / k
-        used = ['0'] * len(nums)
+        taken = ['0'] * n
+        target = total // k
         memo = {}
         
-        def backtrack(i, k, subset_sum):
-            taken_string = ''.join(used)
+        def backtrack(index, count, curr_sum):
+            n = len(nums)
+            taken_string = ''.join(taken)
+            if count == k - 1:
+                return True
+            if curr_sum > target:
+                return False
             if taken_string in memo:
                 return memo[taken_string]
-            if k == 0:
-                return True
-            if subset_sum == target:
-                memo[taken_string] = backtrack(0, k - 1, 0)
-                return memo[taken_string] 
-            
-            for j in range(i, len(nums)):
-                if used[j] == '1' or nums[j] + subset_sum > target:
-                    continue
-                used[j] = '1'
-                if backtrack(j + 1, k, subset_sum + nums[j]):
-                    return True
-                used[j] = '0'
-            
+            if curr_sum == target:
+                memo[taken_string] = backtrack(0, count + 1, 0) 
+                return memo[taken_string]
+            for j in range(index, n):
+                if taken[j] == '0':
+                    taken[j] = '1'
+                    if backtrack(index + 1, count, curr_sum + nums[j]):
+                        return True
+                    taken[j] = '0'
             memo[taken_string] = False
             return memo[taken_string]
         
-        return backtrack(0, k, 0)
-                
+        return backtrack(0, 0, 0)
