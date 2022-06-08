1fd66b7bb3fdef234f2b104ad13ab5115da191a4
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-08 19:10:06 -0400

    Time: 37 ms (98.54%), Space: 14.4 MB (65.90%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,19 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        charCount = [0] * 26
-        
-        def charToIndex(c):
-            return ord(c) - ord('a')
-        
-        if len(s) != len(t):
-            return False
-        
-        for i in range(len(s)):
-            charCount[charToIndex(s[i])] += 1
-            charCount[charToIndex(t[i])] -= 1
-        
-        for count in charCount:
-            if count != 0:
-                return False
-        
-        return True
+        return Counter(s) == Counter(t)
