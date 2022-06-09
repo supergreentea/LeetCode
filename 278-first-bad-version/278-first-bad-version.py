a67162742e1ee4f68f689bf9dcfe64cbacf9f187
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-09 00:00:56 -0400

    Time: 41 ms (52.04%), Space: 13.8 MB (60.36%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -5,7 +5,7 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         l, r = 1, n
         while l < r:
-            m = l + (r - l) // 2
+            m = (l + r) // 2
             if isBadVersion(m):
                 r = m
             else:
