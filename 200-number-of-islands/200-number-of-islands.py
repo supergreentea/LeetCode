a9e9fd9c862e900d8ea3b3ad644beca27500a8a8
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-09 00:54:14 -0400

    Time: 491 ms (33.18%), Space: 21.8 MB (24.21%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,25 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        maxRows, maxCols = len(grid), len(grid[0])
-        visited = set()
-        count = 0
         
         def bfs(row, col):
-            queue = deque([(row, col)])
+            visited.add((row, col))
             directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            queue = deque([(row, col)])
             while queue:
                 r, c = queue.popleft()
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < maxRows and 0 <= nc < maxCols and grid[nr][nc] == '1' and (nr, nc) not in visited:
-                        queue.append((nr, nc))
-                        visited.add((nr, nc))
+                for d_r, d_c in directions:
+                    n_r, n_c = r + d_r, c + d_c
+                    if 0 <= n_r < len(grid) and 0 <= n_c < len(grid[0]) and grid[n_r][n_c] == '1' and (n_r, n_c) not in visited:
+                        visited.add((n_r, n_c))
+                        queue.append((n_r, n_c))
         
-        for i in range(maxRows):
-            for j in range(maxCols):
-                if grid[i][j] == '1' and (i, j) not in visited:
-                    bfs(i, j)
+        count = 0
+        visited = set()
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == '1' and (r, c) not in visited:
+                    bfs(r, c)
                     count += 1
-        
         return count
-                
