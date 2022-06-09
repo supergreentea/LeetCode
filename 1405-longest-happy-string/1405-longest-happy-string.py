4c9b4b6d5252413f8dea7828346a3adc3a99d451
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-09 01:12:55 -0400

    Time: 43 ms (52.09%), Space: 13.9 MB (44.00%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -4,43 +4,38 @@ class Freq:
         self.char, self.count = char, count
         
     def __lt__(self, other):
-        return self.count > other.count
+        return other.count < self.count
 
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        heap = []
-        heapq.heapify(heap)
+        max_heap = []
+        heapq.heapify(max_heap)
         if a > 0:
-            heapq.heappush(heap, Freq("a", a))
+            heapq.heappush(max_heap, Freq("a", a))
         if b > 0:
-            heapq.heappush(heap, Freq("b", b))
+            heapq.heappush(max_heap, Freq("b", b))
         if c > 0:
-            heapq.heappush(heap, Freq("c", c))
+            heapq.heappush(max_heap, Freq("c", c))
         
-        res = ""
-        
-        while heap:
-            cur = heapq.heappop(heap)
-            if len(res) > 0 and res[-1] == cur.char: 
-                if not heap:
-                    return res
-                t = cur
-                cur = heapq.heappop(heap) # pop 2nd most frequent
-                res += cur.char
-                cur.count -= 1
-                if cur.count > 0:
-                    heapq.heappush(heap, cur)
-                heapq.heappush(heap, t)
+        output = ""
+        while max_heap:
+            c = heapq.heappop(max_heap)
+            if len(output) > 1 and c.char == output[-1]:
+                if len(max_heap) == 0:
+                    return output
+                next_c = heapq.heappop(max_heap)
+                output += next_c.char
+                next_c.count -= 1
+                if next_c.count > 0:
+                    heapq.heappush(max_heap, next_c)
+                heapq.heappush(max_heap, c)
             else:
-                if cur.count >= 2:
-                    res += cur.char * 2
-                    cur.count -= 2
+                if c.count >= 2:
+                    output += c.char * 2
+                    c.count -= 2
                 else:
-                    res += cur.char
-                    cur.count -= 1
-                if cur.count > 0:
-                    heapq.heappush(heap, cur)
-        
-        return res
-                
-        
+                    output += c.char
+                    c.count -= 1
+                if c.count > 0:
+                    heapq.heappush(max_heap, c)
+        return output
