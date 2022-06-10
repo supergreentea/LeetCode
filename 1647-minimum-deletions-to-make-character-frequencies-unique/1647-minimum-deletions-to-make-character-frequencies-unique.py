7f543c05935dcc83d88d024f9f2ac8e4f76354fe
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-09 21:26:46 -0400

    Time: 315 ms (35.96%), Space: 14.7 MB (94.40%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,21 +1,19 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
-        
         frequencies = [0] * 26
-        
         for c in s:
             frequencies[ord(c) - ord('a')] += 1
         
         frequencies.sort(reverse = True)
         
-        max_allowed_frequency = len(s)
+        max_allowed_freq = len(s)
         
         deletions_count = 0
         
         for freq in frequencies:
-            if freq > max_allowed_frequency:
-                deletions_count += freq - max_allowed_frequency
-                freq = max_allowed_frequency
-            max_allowed_frequency = max(0, freq - 1)
+            if freq > max_allowed_freq:
+                deletions_count += freq - max_allowed_freq
+                freq = max_allowed_freq
+            max_allowed_freq = max(0, freq - 1)
         
         return deletions_count
