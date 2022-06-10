1efede0001712898777b99c6bdbf2451addc00fb
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-09 21:42:35 -0400

    Time: 1172 ms (65.69%), Space: 13.9 MB (59.96%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -6,15 +6,13 @@ class Solution:
             nonlocal start
             nonlocal end
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r - l > end - start:
+                if r - l >= end - start:
                     start, end = l, r
-                l-= 1
+                l -= 1
                 r += 1
-        
+                
         for i in range(len(s)):
             expand(i, i)
-            expand(i, i + 1)
+            expand(i, i + 1) # for even length palindromes
         
         return s[start : end + 1]
-        
-        
