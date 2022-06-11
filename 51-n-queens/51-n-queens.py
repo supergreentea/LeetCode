0ce2a3a5195071a5c85974ca478597567592f70b
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-11 01:10:05 -0400

    Time: 82 ms (64.18%), Space: 14.5 MB (43.55%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,34 +1,34 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.'] * n for _ in range(n)]
+        cols, diag, antidiag = set(), set(), set()
+        output = []
         
         def backtrack(row):
             if row == n:
                 solution = []
                 for r in board:
-                    solution.append("".join(r))
+                    solution.append(''.join(r))
                 output.append(solution)
+                return
             
-            for col in range(0, n):
-                if col in cols or row - col in diag or row + col in antidiag:
+            for col in range(n):
+                if col in cols or row + col in diag or row - col in antidiag:
                     continue
-
+                
                 # place queen
                 board[row][col] = 'Q'
                 cols.add(col)
-                diag.add(row - col)
-                antidiag.add(row + col)
+                diag.add(row + col)
+                antidiag.add(row - col)
                 
                 backtrack(row + 1)
                 
-                # remove queen to try placing in next column
+                # remove queen
                 board[row][col] = '.'
                 cols.remove(col)
-                diag.remove(row - col)
-                antidiag.remove(row + col)
-            
-        board = [['.'] * n for _ in range(n)]
-        cols, diag, antidiag = set(), set(), set()
-        output = []
+                diag.remove(row + col)
+                antidiag.remove(row - col)
+        
         backtrack(0)
         return output
-                
