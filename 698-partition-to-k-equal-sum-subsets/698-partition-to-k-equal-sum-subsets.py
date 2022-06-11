95787a8cd9dbf06b0154cf9f8c99b550479abff6
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-10 22:16:39 -0400

    Time: 1353 ms (16.75%), Space: 42.4 MB (11.69%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -4,29 +4,28 @@ class Solution:
         n = len(nums)
         if total % k != 0:
             return False
-        memo = {}
-        taken = ['0'] * n
+        used = ['0'] * n
         target = total // k
+        memo = {}
         
         def backtrack(index, count, curr_sum):
-            taken_str = ''.join(taken)
+            used_str = ''.join(used)
             if count == k - 1:
                 return True
             if curr_sum > target:
                 return False
-            if taken_str in memo:
-                return memo[taken_str]
+            if used_str in memo:
+                return memo[used_str]
             if curr_sum == target:
-                memo[taken_str] = backtrack(0, count + 1, 0)
-                return memo[taken_str]
-            for j in range(index, n):
-                if taken[j] == '0':
-                    taken[j] = '1'
-                    if backtrack(index + 1, count, curr_sum + nums[j]):
+                memo[used_str] = backtrack(0, count + 1, 0)
+                return memo[used_str]
+            for i in range(index, n):
+                if used[i] == '0':
+                    used[i] = '1'
+                    if backtrack(index + 1, count, curr_sum + nums[i]):
                         return True
-                    taken[j] = '0'
-            memo[taken_str] = False
-            return memo[taken_str]
+                    used[i] = '0'
+            memo[used_str] = False
+            return False
         
         return backtrack(0, 0, 0)
-            
