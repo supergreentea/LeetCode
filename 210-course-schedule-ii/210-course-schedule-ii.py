e4eb3568f6b34d8fcf243f54f6d3496f461b817a
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-11 15:40:44 -0400

    Time: 229 ms (10.29%), Space: 17.6 MB (19.09%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prereqs = defaultdict(list)
+        for crs, pre in prerequisites:
+            prereqs[crs].append(pre)
+        on_path, explored = set(), set()
+        ordering = []
+        
+        def dfs(crs):
+            if crs in on_path:
+                return False
+            if crs in explored:
+                return True
+            on_path.add(crs)
+            for pre in prereqs[crs]:
+                if not dfs(pre):
+                    return False
+            on_path.remove(crs)
+            explored.add(crs)
+            ordering.append(crs)
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return []
+        
+        return ordering
+            
