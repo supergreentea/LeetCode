707990e0c280ea85ffabc0a5b96f95f9ebe9c9bb
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-10 22:59:44 -0400

    Time: 49 ms (39.62%), Space: 13.7 MB (98.07%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,23 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
+        
         if n == 1:
             return nums[0]
-        if n == 0:
-            return 0
         
-        # solve for 0 to n - 2
-        r1, r2 = 0, 0
-        
+        # rob 0 to n - 2
+        r1 = r2 = 0
         for i in range(0, n - 1):
             r = max(r1 + nums[i], r2)
             r1, r2 = r2, r
         
-        # solve for 1 to n - 1
-        t1, t2 = 0, 0
+        # rob 1 to n - 1
+        t1 = t2 = 0
         for i in range(1, n):
             t = max(t1 + nums[i], t2)
             t1, t2 = t2, t
-        
-        # find max of these
+         
         return max(r, t)
