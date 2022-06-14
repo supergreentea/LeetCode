c72bdc15ec26c58ea55a67bfdccf206253248080
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-13 23:11:42 -0400

    Time: 1275 ms (48.55%), Space: 18.3 MB (22.51%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,22 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """
+        - sort the array
+        - reduce problem to 2sum 
+        - loop through array
+        - for each number, look for pair such that the three numbers sum up to 0
+        - we can use two pointers for the pair, left starting right after current number and other at end of array
+        - we can calibrate sum using pointers and using sorted property of array:
+            - to increase the sum, we can move left pointer to right
+            - to decrease sum, we can move right pointer to left
+        """
         nums.sort()
-        res = []
-        
-        for i in range(len(nums) - 2):
+        n = len(nums)
+        triplets = []
+        for i in range(n - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            l, r = i + 1, len(nums) - 1
+            l, r = i + 1, n - 1
             while l < r:
-                three_sum = nums[i] + nums[l] + nums[r]
-                if three_sum < 0:
+                t = nums[i] + nums[l] + nums[r]
+                if t < 0:
                     l += 1
-                elif three_sum > 0:
+                elif t > 0:
                     r -= 1
                 else:
-                    res.append([nums[i], nums[l], nums[r]])
+                    triplets.append([nums[i], nums[l], nums[r]])
                     l += 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-        
-        return res
+        return triplets
