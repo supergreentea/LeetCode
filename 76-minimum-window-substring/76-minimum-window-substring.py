c2363a905bf2e8a0e6f9ede01ce688ce99177e81
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-15 22:18:39 -0400

    Time: 112 ms (82.24%), Space: 14.7 MB (36.06%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,3 +1,25 @@
+"""
+Time complexity: O(|S| + |T|) where |S| and |T| represent the lengths of strings S and T. 
+- We need to iterate through all elements of T to form the dictionary.
+- We need to iterate through all the elements of S at most 2 times, once with left pointer and once with right pointer.
+Space complexity: O(|S| +|T|)
+- The dictionary sizes scale with the number of unique elements in S and T, which are at most |S| and |T|
+"""
+
+"""
+Strategy:
+Sliding window algorithm:
+    - use two pointers, l and r (denoting the left side and right sides of the sliding window)
+    - move r until we know the window has all the characters in t. update our result indices (indices indicating start and end of substring answer)
+    - when window has all characters in t, move left pointer to shrink window until that condition is false, then expand window again by moving r 
+    - to find new substring fulfilling condition
+
+Maintain two dictionaries
+window_count : frequency of characters in current window
+t_count: frequency of characters in string t
+"""
+
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(s) < len(t) or s == "":
@@ -24,6 +46,7 @@ class Solution:
                         if s[l] in t_count and window_count[s[l]] == t_count[s[l]] - 1:
                             have -= 1
                         l += 1
+                        
         if res_right == len(s):
             return ""
         
