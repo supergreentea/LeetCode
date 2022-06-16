134265c4b4418e945cd20afd8b6bf56df1891f63
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-15 22:09:00 -0400

    Time: 147 ms (57.63%), Space: 14.8 MB (36.06%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,33 +1,33 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
+        if len(s) < len(t) or s == "":
             return ""
-        """
-        Strategy:
-        Use a sliding window approach.
-        Use two pointers, l and r both starting at 0.
-        Extend r until t is included in window.
-        As long as t is included in window, we move l to decrease window until t is no longer included in window; we move r again until t is included again.
-        We update the pointers for the minimum window substring while this is happening.
-        We can use a Counter to keep track of frequency of characters in window.
-        We can use a Counter with the frequency of characters in t for comparison.
-        If the intersection of the two counters is the counter for t, then we know t is in that window.
-        We can return return the min window substring using the pointers we kept track of.
-        """
-        l = r = 0
-        ans_l, ans_r = 0, len(s)
-        t_counter = Counter(t)
-        window_counter = Counter()
-        while r < len(s):
-            window_counter[s[r]] += 1
-            while t_counter & window_counter == t_counter:
-                if r - l < ans_r - ans_l:
-                    ans_l, ans_r = l, r
-                window_counter[s[l]] -= 1
-                if window_counter[s[l]] == 0:
-                    del window_counter[s[l]]
-                l += 1
-            r += 1
-        if ans_r == len(s):
+        
+        window_count, t_count = defaultdict(int), defaultdict(int)
+        
+        for c in t:
+            t_count[c] += 1
+        
+        have, need = 0, len(t_count)
+        
+        res_left, res_right = 0, len(s)
+        
+        l = 0
+        for r in range(len(s)):
+            window_count[s[r]] += 1
+            if s[r] in t_count:
+                if window_count[s[r]] == t_count[s[r]]:
+                    have += 1
+                    while have == need:
+                        if r - l < res_right - res_left:
+                            res_left, res_right = l, r
+                        window_count[s[l]] -= 1
+                        if s[l] in t_count and window_count[s[l]] == t_count[s[l]] - 1:
+                            have -= 1
+                        l += 1
+        if res_right == len(s):
             return ""
-        return s[ans_l:ans_r + 1]
+        return s[res_left : res_right + 1]
+        
+        
+        
