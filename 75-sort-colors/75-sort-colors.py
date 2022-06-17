00c1cb991c45cc6e18f2f38b84de9cac64bd2e89
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-16 23:29:30 -0400

    Time: 39 ms (79.21%), Space: 13.9 MB (62.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,19 +4,19 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        red_index, blue_index = 0, n - 1
+        red_end, blue_start = 0, n - 1
         
         for color in nums:
             if color == 0:
-                red_index += 1
+                red_end += 1
             elif color == 2:
-                blue_index -= 1
+                blue_start -= 1
         
-        for i in range(0, red_index):
+        for i in range(0, red_end):
             nums[i] = 0
         
-        for i in range(red_index, blue_index + 1):
+        for i in range(red_end, blue_start + 1):
             nums[i] = 1
         
-        for i in range(blue_index + 1, n):
+        for i in range(blue_start + 1, n):
             nums[i] = 2
