08217b4f612c18ee81cc0f09f765935f041fbe78
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-17 01:09:49 -0400

    Time: 9545 ms (6.46%), Space: 14 MB (12.18%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,25 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        n = len(word)
         ROWS, COLS = len(board), len(board[0])
         visited = set()
         
-        def backtrack(r, c, i):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or board[r][c] != word[i]:
+        def backtrack(row, col, word_index):
+            if row < 0 or row >= ROWS or col < 0 or col >= COLS or board[row][col] != word[word_index]:
                 return False
-            if i == len(word) - 1:
+            if word_index == n - 1:
                 return True
-            offsets = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-            visited.add((r, c))
-            for r_offset, c_offset in offsets:
-                if backtrack(r + r_offset, c + c_offset, i + 1):
+            
+            visited.add((row, col))
+            for row_offset, col_offset in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                new_row, new_col = row + row_offset, col + col_offset
+                if (new_row, new_col) in visited:
+                    continue
+                if backtrack(new_row, new_col, word_index + 1):
                     return True
-            visited.remove((r, c))
+            visited.remove((row, col))
             return False
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if backtrack(r, c, 0):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if backtrack(row, col, 0):
                     return True
         
         return False
-                
+            
+            
