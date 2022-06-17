6bd4502c2d69aec9affe1863d75e98bae13cf9f6
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-16 23:28:36 -0400

    Time: 33 ms (93.41%), Space: 13.9 MB (62.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,26 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        red_count = white_count = blue_count = 0
+        n = len(nums)
+        red_index, blue_index = 0, n - 1
         
         for color in nums:
             if color == 0:
-                red_count += 1
-            elif color == 1:
-                white_count += 1
-            else:
-                blue_count += 1
+                red_index += 1
+            elif color == 2:
+                blue_index -= 1
         
-        index = 0
-        for _ in range(red_count):
-            nums[index] = 0
-            index += 1
-        for _ in range(white_count):
-            nums[index] = 1
-            index += 1
-        for _ in range(blue_count):
-            nums[index] = 2
-            index += 1
+        for i in range(0, red_index):
+            nums[i] = 0
         
+        for i in range(red_index, blue_index + 1):
+            nums[i] = 1
         
-        
+        for i in range(blue_index + 1, n):
+            nums[i] = 2
