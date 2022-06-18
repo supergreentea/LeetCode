c0325df5dd7934b4fa8ea41c3f15c4fbdf669729
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-18 01:36:12 -0400

    Time: 236 ms (10.29%), Space: 17.7 MB (10.44%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,12 +1,14 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # build adjacency list representation
         prereqs = defaultdict(list)
         
         for crs, pre in prerequisites:
             prereqs[crs].append(pre)
         
         on_path, explored = set(), set()
-        ordering = []
+        
+        topological_ordering = []
         
         def dfs(crs):
             if crs in on_path:
@@ -18,13 +20,15 @@ class Solution:
             for pre in prereqs[crs]:
                 if not dfs(pre):
                     return False
+                
             on_path.remove(crs)
             explored.add(crs)
-            ordering.append(crs)
+            topological_ordering.append(crs)
             return True
         
         for crs in range(numCourses):
             if not dfs(crs):
                 return []
         
-        return ordering
+        return topological_ordering
+        
