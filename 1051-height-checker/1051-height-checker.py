11a930436d2a3f9fe1a05568affed6066c084eb6
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-19 23:47:55 -0400

    Time: 61 ms (33.23%), Space: 14 MB (10.07%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,18 +1,26 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         copy = heights.copy()
+        
         counts = [0] * 101
+        
         for height in heights:
             counts[height] += 1
-        i = 0
-        for j in range(101):
-            while counts[j] > 0:
-                copy[i] = j
-                i += 1
-                counts[j] -= 1
-        ans = 0
-        for i in range(len(heights)):
-            if copy[i] != heights[i]:
-                ans += 1
-        return ans
         
+        num_previous_items = 0
+        for i in range(101):
+            count = counts[i]
+            counts[i] = num_previous_items
+            num_previous_items += count
+        
+        for height in heights:
+            copy[counts[height]] = height
+            counts[height] += 1
+        
+        ans = 0
+        
+        for i in range(len(heights)):
+            if heights[i] != copy[i]:
+                ans += 1
+        
+        return ans
