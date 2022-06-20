7caee3723fcf6376b3dd0324e9d9ea5bfe70a821
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-19 23:38:10 -0400

    Time: 547 ms (79.13%), Space: 22.6 MB (26.82%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,23 +1,19 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
+        counts = [0] * 100001
+        for num in nums:
+            counts[num + 50000] += 1
         
-        def quicksort(l, r):
-            if l < r:
-                pi = partition(l, r)
-                quicksort(l, pi - 1)
-                quicksort(pi + 1, r)
+        num_items_before = 0
+        for i in range(100001):
+            count = counts[i]
+            counts[i] = num_items_before
+            num_items_before += count
         
-        def partition(l, r):
-            p = randint(l, r)
-            nums[r], nums[p] = nums[p], nums[r]
-            i = l
-            for j in range(l, r):
-                if nums[j] < nums[r]:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    i += 1
-            nums[r], nums[i] = nums[i], nums[r]
-            return i
+        sorted_array = [0] * len(nums)
         
-        quicksort(0, len(nums) - 1)
-        return nums
-            
+        for num in nums:
+            sorted_array[counts[num + 50000]] = num
+            counts[num + 50000] += 1
+        
+        return sorted_array
