0a18dfc215efb92a1f0f670cbedc06615e53e835
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-21 15:58:35 -0400

    Time: 28 ms (98.97%), Space: 14.1 MB (80.65%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         output = []
         
-        def backtrack(index = 0, subset = []):
-            output.append(subset.copy())
-            for i in range(index, len(nums)):
-                subset.append(nums[i])
-                backtrack(i + 1, subset)
-                subset.pop()
-                
-        backtrack()
+        def backtrack(index, subset):
+            if index == n:
+                output.append(subset.copy())
+                return
+            subset.append(nums[index])
+            backtrack(index + 1, subset)
+            subset.pop()
+            backtrack(index + 1, subset)
         
+        backtrack(0, [])
         return output
-        
