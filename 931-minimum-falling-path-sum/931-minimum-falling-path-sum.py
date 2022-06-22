bf0368d0a51989134e4e1f78efe8d2fd9e254043
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-21 23:50:37 -0400

    Time: 252 ms (27.63%), Space: 14.9 MB (40.31%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -1,12 +1,11 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         ROWS, COLS = len(matrix), len(matrix[0])
-        for r in range(1, ROWS):
-            for c in range(COLS):
-                if c == 0:
-                    matrix[r][c] += min(matrix[r-1][c], matrix[r-1][c+1])
-                elif c == COLS - 1:
-                    matrix[r][c] += min(matrix[r-1][c], matrix[r-1][c-1])
-                else:
-                    matrix[r][c] += min(matrix[r-1][c], matrix[r-1][c+1], matrix[r-1][c-1])
-        return min(matrix[-1])
+        
+        for row in range(1, ROWS):
+            matrix[row][0] += min(matrix[row-1][0], matrix[row-1][1])
+            for col in range(1, COLS - 1):
+                matrix[row][col] += min(matrix[row-1][col-1], matrix[row-1][col], matrix[row-1][col+1])
+            matrix[row][COLS-1] += min(matrix[row-1][COLS-1], matrix[row-1][COLS-2])
+        
+        return min(matrix[ROWS-1])
