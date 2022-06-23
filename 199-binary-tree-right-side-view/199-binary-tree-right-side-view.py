4f7307633efd11815adcf7c6950a73e9700aedd6
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-23 02:55:17 -0400

    Time: 56 ms (30.71%), Space: 13.9 MB (22.82%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -6,7 +6,9 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-    
+        
+        output = []
+        
         def dfs(root, level):
             if not root:
                 return
@@ -15,8 +17,6 @@ class Solution:
             dfs(root.right, level + 1)
             dfs(root.left, level + 1)
         
-        if not root:
-            return []
-        output = []
         dfs(root, 0)
+        
         return output
