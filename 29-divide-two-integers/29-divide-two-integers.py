994665769290ac18b68a1dc403d0b50cc2537414
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-24 02:43:58 -0400

    Time: 47 ms (61.62%), Space: 13.9 MB (75.54%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -1,32 +1,31 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         
-        MAX_INT, MIN_INT = 2147483647, -2147483648
+        MAX_INT, MIN_INT = 2 ** 31 - 1, -1 * 2 ** 31
         
-        different_signs = (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0)
+        # check whether inputs are different signs
+        different_signs = (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0)
         
+        # make signs positive for both 
         if dividend < 0:
             dividend = 0 - dividend
         if divisor < 0:
             divisor = 0 - divisor
         
-        s = 0
         quotient = 0
         while dividend >= divisor:
             power_of_two = 1
             value = divisor
             
             while value << 1 < dividend:
-                value <<= 1
+                value <<= 1 
                 power_of_two <<= 1
             
             quotient += power_of_two
             dividend -= value
-            
         
         if different_signs:
             quotient = 0 - quotient
             return max(quotient, MIN_INT)
         
         return min(quotient, MAX_INT)
-            
