c2866dfafdf7559cd892036d3eaf3b5ba18d4dd0
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-26 01:41:24 -0400

    Time: 131 ms (5.24%), Space: 14.2 MB (5.74%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,19 +1,31 @@
 class Solution:
-    def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        counts = Counter(words)
-        buckets = [[] for _ in range(len(words) + 1)]
-        
-        for word, count in counts.items():
-            buckets[count].append(word)
             
-        for bucket in buckets:
-            bucket.sort()
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
         
-        output = []
-        for i in range(len(words), -1 , -1):
-            for word in buckets[i]:
-                output.append(word)
-                if len(output) == k:
-                    return output
-                
-        return output
+        class Word:
+        
+            def __init__(self, word, freq):
+                self.word, self.freq = word, freq
+
+            def __lt__(self, other):
+                if self.freq == other.freq:
+                    return self.word > other.word
+                return self.freq < other.freq
+
+            def __eq__(self, other):
+                return self.freq == other.freq and self.word == other.word
+        
+        heap = []
+        counter = collections.Counter(words)
+        for word, count in counter.items():
+            heapq.heappush(heap, Word(word, count))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(heap).word)
+        
+        return res[::-1]
+        
+    
+    
