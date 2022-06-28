523feb159c8124ceaf892b589b51c981925fb3ed
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-28 01:08:41 -0400

    Time: 145 ms (78.16%), Space: 15.2 MB (32.42%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -8,16 +8,19 @@ class Solution:
         ans = []
         for window_right in range(len(s)):
             cur_char = s[window_right]
+            
             if window_right - window_left + 1 > len(p):
                 char_left = s[window_left]
                 if s_counter[char_left] == p_counter[char_left]:
                     formed -= 1
                 s_counter[char_left] -= 1
                 window_left += 1
+                
             if s_counter[cur_char] == p_counter[cur_char] - 1:
                 formed += 1
                 if formed == keys:
                     ans.append(window_left)
+                    
             s_counter[cur_char] += 1
         return ans
             
