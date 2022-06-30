3d84c7042bbfe09b7581599ec889e4d404e70b4e
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-06-29 21:10:18 -0400

    Time: 9012 ms (18.37%), Space: 13.9 MB (87.43%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -1,20 +1,22 @@
 class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        visited = set()
         max_gold = 0
+        visited = set()
         
         def backtrack(row, col, gold):
-            nonlocal max_gold
-            if row < 0 or row >= ROWS or col < 0 or col >= COLS or grid[row][col] == 0 or (row, col) in visited:
+            if row < 0 or row >= ROWS or \
+               col < 0 or col >= COLS or \
+               (row, col) in visited or \
+               grid[row][col] == 0:
                 return
-            
+            nonlocal max_gold
             gold += grid[row][col]
-            max_gold = max(gold, max_gold)
+            max_gold = max(max_gold, gold)
             visited.add((row, col))
-            for row_offset, col_offset in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                new_row, new_col = row + row_offset, col + col_offset
-                backtrack(new_row, new_col, gold)
+            for row_offset, col_offset in [(0,1), (0, -1), (1, 0), (-1, 0)]:
+                next_row, next_col = row + row_offset, col + col_offset
+                backtrack(next_row, next_col, gold)
             visited.remove((row, col))
         
         for row in range(ROWS):
