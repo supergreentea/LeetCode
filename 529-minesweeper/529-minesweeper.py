fbc03ec1e30f501e4899b71a2a0ff35ff1318a19
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-01 17:46:28 -0400

    Time: 407 ms (5.85%), Space: 16.8 MB (50.23%) - LeetHub

--- 529-minesweeper/529-minesweeper.py
+++ 529-minesweeper/529-minesweeper.py
@@ -1,28 +1,30 @@
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        adjacent = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
         ROWS, COLS = len(board), len(board[0])
-        row, col = click[0], click[1]
-        if board[row][col] == 'M':
-            board[row][col] = 'X'
-            return board
         
-        def perform_click(row, col):
-            num_adjacent_mines = 0
+        def on_board(row, col):
+            return row >= 0 and row < ROWS and col >= 0 and col < COLS
+        
+        def reveal(row, col):
+            if board[row][col] == 'M':
+                board[row][col] = 'X'
+                return
             
-            for row_offset, col_offset in [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]:
-                adj_row, adj_col = row + row_offset, col + col_offset
-                if 0 <= adj_row < ROWS and 0 <= adj_col < COLS and board[adj_row][adj_col] == 'M':
-                    num_adjacent_mines += 1
-                
-            if num_adjacent_mines == 0:
-                board[row][col] = 'B'
-                for row_offset, col_offset in [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]:
+            if board[row][col] == 'E':
+                num_mines = 0
+                for row_offset, col_offset in adjacent:
                     adj_row, adj_col = row + row_offset, col + col_offset
-                    if 0 <= adj_row < ROWS and 0 <= adj_col < COLS and board[adj_row][adj_col] == 'E':
-                        perform_click(adj_row, adj_col)
-            else:
-                board[row][col] = str(num_adjacent_mines)
-            
-            
-        perform_click(row, col)
-        return board    
+                    if on_board(adj_row, adj_col) and board[adj_row][adj_col] == 'M':
+                        num_mines += 1
+                if num_mines > 0:
+                    board[row][col] = str(num_mines)
+                else:
+                    board[row][col] = 'B'
+                    for row_offset, col_offset in adjacent:
+                        adj_row, adj_col = row + row_offset, col + col_offset
+                        if on_board(adj_row, adj_col):
+                            reveal(adj_row, adj_col)
+        
+        reveal(click[0], click[1])
+        return board
