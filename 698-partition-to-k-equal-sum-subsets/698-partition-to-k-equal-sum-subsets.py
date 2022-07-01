d244219d351d9fd2747ce7de6b9141be952eabce
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-01 17:30:42 -0400

    Time: 587 ms (43.54%), Space: 46.1 MB (9.02%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -1,20 +1,17 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        used = ['0'] * len(nums)
         total_sum = sum(nums)
         if total_sum % k != 0:
             return False
-        
         target_sum = total_sum // k
-        
-        used = ['0'] * len(nums)
-        
         memo = {}
         
-        def backtrack(index, subsets_formed, current_sum):
+        def backtrack(index, count, current_sum):
+            if count == k - 1:
+                return True
             if current_sum > target_sum:
                 return False
-            if subsets_formed == k - 1:
-                return True
             
             memo_string = ''.join(used)
             
@@ -22,13 +19,13 @@ class Solution:
                 return memo[memo_string]
             
             if current_sum == target_sum:
-                memo[memo_string] = backtrack(0, subsets_formed + 1, 0)
+                memo[memo_string] = backtrack(0, count + 1, 0)
                 return memo[memo_string]
             
             for i in range(index, len(nums)):
                 if used[i] == '0':
                     used[i] = '1'
-                    if backtrack(i + 1, subsets_formed, current_sum + nums[i]):
+                    if backtrack(i + 1, count, current_sum + nums[i]):
                         return True
                     used[i] = '0'
             
@@ -36,3 +33,4 @@ class Solution:
             return False
         
         return backtrack(0, 0, 0)
+            
