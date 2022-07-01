ab1d8bf353c102c11309129fb28c8987c2808d7d
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-01 16:24:32 -0400

    Time: 1016 ms (72.75%), Space: 76.7 MB (10.19%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -3,48 +3,44 @@ class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
         self.prev = self.next = None
-    
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        
-        # left.next would next LRU, right.prev would be MRU
+        self.cache = {}
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
         
-        self.cache = {} # key is key, value is node
+        self.capacity = capacity
     
-    def remove_node(self, node):
+    # remove node from linked list
+    def remove(self, node):
         prev, nxt = node.prev, node.next
-        node.prev.next, node.next.prev = nxt, prev
-    
-    def add_node(self, node):
-        mru = self.right.prev
+        prev.next, nxt.prev = nxt, prev
         
-        mru.next = node
-        node.prev = mru
+    # insert node to right of linked list
+    def insert(self, node):
+        prev = self.right.prev
+        prev.next = self.right.prev = node
+        node.prev, node.next = prev, self.right
         
-        self.right.prev = node
-        node.next = self.right
         
-
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove_node(self.cache[key])
-            self.add_node(self.cache[key])
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
             return self.cache[key].val
         return -1
         
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove_node(self.cache[key])
+            self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
-        self.add_node(self.cache[key])
+        self.insert(self.cache[key])
+        
         if len(self.cache) > self.capacity:
             lru = self.left.next
-            self.remove_node(lru)
+            self.remove(lru)
             del self.cache[lru.key]
 
 
