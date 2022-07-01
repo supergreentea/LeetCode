84568cdec99a4cf346c7d85d1316fd6b082626bc
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-01 17:12:00 -0400

    Time: 80 ms (86.66%), Space: 14.1 MB (31.25%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,27 +1,31 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_counter = Counter(s1)
-        window_counter = Counter()
-        window_left = 0
-        completed = 0
         
-        for window_right in range(len(s2)):
-            cur_char = s2[window_right]
-            if not cur_char in s1_counter:
-                window_left = window_right + 1
-                window_counter = Counter()
-                completed = 0
-                continue
-            window_counter[cur_char] += 1
-            if window_counter[cur_char] == s1_counter[cur_char]:
-                completed += 1
-                if completed == len(s1_counter):
-                    return True
-            while window_counter[cur_char] > s1_counter[cur_char]:
-                left_char = s2[window_left]
-                window_counter[left_char] -= 1
-                if window_counter[left_char] == s1_counter[left_char] - 1:
-                    completed -= 1
-                window_left += 1
+        def matches():
+            for i in range(26):
+                if s1_count[i] != s2_count[i]:
+                    return False
+            return True
+        
+        if len(s1) > len(s2):
+            return False
+        
+        s1_count = [0] * 26
+        s2_count = [0] * 26
+        
+        for i in range(len(s1)):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
+        
+        if matches():
+            return True
+        
+        for i in range(len(s2) - len(s1)):
+            s2_count[ord(s2[len(s1) + i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] -= 1
+            if matches():
+                return True
         
         return False
+        
+        
