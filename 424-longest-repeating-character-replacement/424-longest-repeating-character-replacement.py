d8108f092401f3fea56780045096471758cc05ce
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-01 21:40:09 -0400

    Time: 285 ms (24.18%), Space: 13.9 MB (93.19%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,20 +1,14 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        count = defaultdict(int)
+        res = 0
         
-        # replacements needed in a substring = length substring - most frequent char in substring
-        max_length = 1
-        i = 0
-        n = len(s)
-        char_freq = defaultdict(int)
-        for j in range(n):
-            c = s[j]
-            char_freq[c] += 1
-            replacements = j - i + 1 - max(char_freq.values())
-            while replacements > k:
-                char_freq[s[i]] -= 1
-                i += 1
-                replacements = j - i + 1 - max(char_freq.values())
-            max_length = max(max_length, j - i + 1)
-        return max_length
-                
-        
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] += 1
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            
+            res = max(res, r - l + 1)
+        return res
