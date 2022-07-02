57446b03a6830baec4cec8759630fd422cc7b61a
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-01 22:26:32 -0400

    Time: 52 ms (41.37%), Space: 13.9 MB (71.15%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,20 +1,16 @@
-"""
-Time complexity: O(N) where N is |nums|
-Space complexity: O(1) no extra space
-
-"""
-
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
+        
         r1 = r2 = 0
-        n = len(nums)
-        for i in range(n - 1):
+        for i in range(len(nums) - 1):
             r = max(r1 + nums[i], r2)
             r1, r2 = r2, r
+        
         s1 = s2 = 0
-        for i in range(1, n):
+        for i in range(1, len(nums)):
             s = max(s1 + nums[i], s2)
             s1, s2 = s2, s
-        return max(r, s)
+        
+        return max(r2, s2)
