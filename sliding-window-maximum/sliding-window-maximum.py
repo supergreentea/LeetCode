593f38342c27ffa2f6027624babb3ca61be33bd8
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-03 03:08:37 -0400

    Time: 3330 ms, Memory: 30.1 MB - LeetHub

---
+++ sliding-window-maximum/sliding-window-maximum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        queue = deque() # indices
+        output = []
+        l = r = 0
+        while r < len(nums):
+            # pop smaller values from queue
+            while queue and nums[queue[-1]] < nums[r]:
+                queue.pop()
+            queue.append(r)
+            
+            # remove left value from window
+            if l > queue[0]:
+                queue.popleft()
+            
+            if (r + 1) >= k:
+                output.append(nums[queue[0]])
+                l += 1
+            r += 1
+        return output
+            
+        
