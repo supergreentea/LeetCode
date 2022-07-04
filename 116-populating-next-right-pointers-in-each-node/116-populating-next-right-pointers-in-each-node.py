ca7a0a27688cebe0e1656cce761aaec19da36f03
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-03 21:56:49 -0400

    Time: 61 ms (96.91%), Space: 15.9 MB (20.82%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -9,10 +9,11 @@ class Node:
 """
 
 class Solution:
-    def connect(self, node: 'Optional[Node]') -> 'Optional[Node]':
-        cur, nxt = node, node.left if node else None
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        cur, nxt = root, root.left if root else None
         while cur and nxt:
             cur.left.next = cur.right
+            
             if cur.next:
                 cur.right.next = cur.next.left
             
@@ -20,4 +21,5 @@ class Solution:
             if not cur:
                 cur = nxt
                 nxt = cur.left
-        return node
+        
+        return root
