d2e4f53379fdd56967c4e0a5a156437fdcd2a72a
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-04 02:12:41 -0400

    Time: 302 ms (26.91%), Space: 16.7 MB (74.92%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -1,15 +1,35 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        ans = 0
         
-        candies = [1] * len(ratings)
+        def count(n):
+            return (n * (n + 1)) // 2
+        
+        if len(ratings) <= 1:
+            return len(ratings)
+        
+        candies = up = down = old_slope = 0
         
         for i in range(1, len(ratings)):
+            new_slope = 0
             if ratings[i] > ratings[i - 1]:
-                candies[i] = candies[i - 1] + 1
+                new_slope = 1
+            if ratings[i] < ratings[i - 1]:
+                new_slope = -1
+            
+            if (old_slope > 0 and new_slope == 0) or (old_slope < 0 and new_slope >= 0):
+                candies += count(up) + count(down) + max(up, down)
+                up = 0
+                down = 0
+            
+            if new_slope > 0:
+                up += 1
+            elif new_slope < 0:
+                down += 1
+            else:
+                candies += 1
+            
+            old_slope = new_slope
         
-        for i in range(len(ratings) - 2, -1, -1):
-            if ratings[i] > ratings[i + 1]:
-                candies[i] = max(candies[i], candies[i + 1] + 1)
-        
-        return sum(candies)
+        candies += count(up) + count(down) + max(up, down) + 1
+        return candies
+            
