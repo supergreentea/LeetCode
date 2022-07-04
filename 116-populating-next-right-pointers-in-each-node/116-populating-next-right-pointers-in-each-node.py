4ba97408738aed17bdda9bcda20c1561d8e3cf1d
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-03 23:25:41 -0400

    Time: 75 ms (77.59%), Space: 15.8 MB (47.74%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -13,13 +13,10 @@ class Solution:
         cur, nxt = root, root.left if root else None
         while cur and nxt:
             cur.left.next = cur.right
-            
             if cur.next:
                 cur.right.next = cur.next.left
-            
             cur = cur.next
             if not cur:
                 cur = nxt
                 nxt = cur.left
-        
         return root
