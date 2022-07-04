2d66f8f718798d2b90beed7502453c1871d0d2a9
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-03 21:52:22 -0400

    Time: 74 ms (79.08%), Space: 15.7 MB (47.74%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -9,25 +9,15 @@ class Node:
 """
 
 class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root:
-            return
-        queue = deque([root])
-        while queue:
-            n = len(queue)
-            node = queue.popleft()
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-            for _ in range(n - 1):
-                next_node = queue.popleft()
-                if next_node.left:
-                    queue.append(next_node.left)
-                if next_node.right:
-                    queue.append(next_node.right)
-                node.next = next_node
-                node = next_node
-            node.next = None
+    def connect(self, node: 'Optional[Node]') -> 'Optional[Node]':
+        cur, nxt = node, node.left if node else None
+        while cur and nxt:
+            cur.left.next = cur.right
+            if cur.next:
+                cur.right.next = cur.next.left
             
-        return root
+            cur = cur.next
+            if not cur:
+                cur = nxt
+                nxt = cur.left
+        return node
