db8af78ef3ea601a61faa39efa7a1155ed4f319d
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-05 00:00:32 -0400

    Time: 81 ms, Memory: 13.9 MB - LeetHub

--- string-compression/string-compression.py
+++ string-compression/string-compression.py
@@ -4,21 +4,21 @@ class Solution:
         
         while fast < len(chars):
             count = 1
-            chars[slow] = chars[fast]
             
-            while fast + 1 < len(chars) and chars[fast + 1] == chars[fast]:
+            chars[slow] = chars[fast] # we can overwrite slow because len(str(count)) <= count
+            
+            while fast + 1 < len(chars) and chars[fast] == chars[fast + 1]:
                 count += 1
                 fast += 1
             
+            # here, fast is pointing to a new character
             
             if count > 1:
                 for c in str(count):
                     chars[slow + 1] = c
                     slow += 1
             
-            slow += 1
+            slow += 1 # slow is pointing to directly after the count of previous character
             fast += 1
         
-        return slow
-            
-            
+        return slow # slow should be at index after last count was processed
