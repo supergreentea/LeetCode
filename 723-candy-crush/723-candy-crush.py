f5ee3b2bce0bd100b020418e300f0254e92113cb
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-05 01:16:43 -0400

    Time: 364 ms (22.56%), Space: 14.1 MB (49.94%) - LeetHub

--- 723-candy-crush/723-candy-crush.py
+++ 723-candy-crush/723-candy-crush.py
@@ -4,25 +4,27 @@ class Solution:
         
         crush = False
         
+        # check horizontal windows of length 3 and mark for crushing
         for row in range(ROWS):
             for col in range(COLS - 2):
                 if abs(board[row][col]) == abs(board[row][col + 1]) == abs(board[row][col + 2]) != 0:
                     board[row][col] = board[row][col + 1] = board[row][col + 2] = -abs(board[row][col])
                     crush = True
         
+        # check vertical windows of length 3 and mark for crushing
         for col in range(COLS):
             for row in range(ROWS - 2):
                 if abs(board[row][col]) == abs(board[row + 1][col]) == abs(board[row + 2][col]) != 0:
                     board[row][col] = board[row + 1][col] = board[row + 2][col] = -abs(board[row][col])
                     crush = True
         
+        # gravity
         for col in range(COLS):
             write_head = ROWS - 1
-            for row in range(ROWS - 1, -1, -1):
+            for row in range(ROWS - 1, -1 , -1):
                 if board[row][col] > 0:
                     board[write_head][col] = board[row][col]
                     write_head -= 1
-            
             for write_head in range(write_head, -1, -1):
                 board[write_head][col] = 0
         
@@ -30,4 +32,4 @@ class Solution:
             return board
         
         return self.candyCrush(board)
-                
+        
