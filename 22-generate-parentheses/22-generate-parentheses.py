4ad46618feeaabdf61fafc5752b5ecce03390a69
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-07 21:04:25 -0400

    Time: 62 ms (32.82%), Space: 14.4 MB (6.40%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        combinations = { 1 : set(["()"]) }
+        
+        if n == 1:
+            return combinations[1]
+        
+        for i in range(2, n + 1):
+            combinations[i] = set()
+            for j in range(1, i):
+                first_set = combinations[j]
+                second_set = combinations[i - j]
+                
+                for first_combination in first_set:
+                    for insert_index in range(len(first_combination) + 1):
+                        for second_combination in second_set:
+                            new_combination = first_combination[:insert_index] + second_combination + first_combination[insert_index:]
+                            combinations[i].add(new_combination)
+                        
+        return combinations[n]
