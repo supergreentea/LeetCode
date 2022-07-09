35e9aaa1f321ed83988ec7b712633ed72ddea44c
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-09 03:30:41 -0400

    Time: 295 ms (50.36%), Space: 17.5 MB (37.56%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -3,24 +3,26 @@ class Solution:
         if endWord not in wordList:
             return 0
         
+        # O(m^2)
         def get_patterns(word):
             patterns = []
-            for i in range(len(word)):
-                patterns.append(word[:i] + "*" + word[i + 1:])
+            for i in range(len(word)): # m operations
+                patterns.append(word[:i] + "*" + word[i + 1:]) # O(m) to create string
             return patterns
         
         wordList.append(beginWord)
         
         graph = defaultdict(list)
         
-        for word in wordList:
-            for pattern in get_patterns(word):
+        for word in wordList: # n words
+            for pattern in get_patterns(word): # m patterns
                 graph[pattern].append(word)
         
         queue = deque([beginWord])
         visited = set([beginWord])
         level = 1
         
+        # 
         while queue:
             for i in range(len(queue)):
                 word = queue.popleft()
