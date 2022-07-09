be4099ed0d7098fa4f7bbe759f1dd8e6837f2858
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-09 04:20:52 -0400

    Time: 36 ms (80.46%), Space: 14 MB (30.29%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,23 +1,31 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        
+        mappings = {
+            '2' : ['a', 'b', 'c'],
+            '3' : ['d', 'e', 'f'],
+            '4' : ['g', 'h', 'i'],
+            '5' : ['j', 'k', 'l'],
+            '6' : ['m', 'n', 'o'],
+            '7' : ['p', 'q', 'r', 's'],
+            '8' : ['t', 'u', 'v'],
+            '9' : ['w', 'x', 'y', 'z']
+        }
+        
         output = []
         
         if not digits:
             return output
         
-        digits_to_letters = { '2': ["a", "b", "c"], '3': ["d", "e", "f"], '4': ["g", "h", "i"], '5': ["j", "k", "l"], '6': ["m", "n", "o"], '7' : ["p", "q", "r", "s"], '8': ["t", "u", "v"], '9': ["w", "x", "y", "z"] }
-        
-        def backtrack(index = 0, stringbuilder = []):
+        def backtrack(index = 0, combination = []):
             if index == len(digits):
-                output.append("".join(stringbuilder))
+                output.append("".join(combination))
                 return
             
-            digit = digits[index]
-            
-            for letter in digits_to_letters[digit]:
-                stringbuilder.append(letter)
-                backtrack(index + 1, stringbuilder)
-                stringbuilder.pop()
-            
+            for letter in mappings[digits[index]]:
+                combination.append(letter)
+                backtrack(index + 1, combination)
+                combination.pop()
+        
         backtrack()
         return output
