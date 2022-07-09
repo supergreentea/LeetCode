e4a2ab540cf6406339593e06c6aedf320674d55a
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-09 04:31:53 -0400

    Time: 308 ms (78.67%), Space: 41.6 MB (9.81%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,13 +1,12 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-
+        
         @cache
-        def count_ways(index = 0, remaining_sum = target):
+        def get_count(index = 0, cur_sum = 0):
             if index == len(nums):
-                if remaining_sum == 0:
+                if cur_sum == target:
                     return 1
                 return 0
-            
-            return count_ways(index + 1, remaining_sum + nums[index]) + count_ways(index + 1, remaining_sum - nums[index])
+            return get_count(index + 1, cur_sum + nums[index]) + get_count(index + 1, cur_sum - nums[index])
         
-        return count_ways()
+        return get_count()
