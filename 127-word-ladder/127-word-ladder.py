02a8bf25060f57e38164f5b6e9ddf2552e33fb09
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-09 04:05:52 -0400

    Time: 254 ms (56.47%), Space: 18.8 MB (13.88%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,40 +1,35 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
         
-        # O(m^2)
-        def get_patterns(word):
-            patterns = []
-            for i in range(len(word)): # m operations
-                patterns.append(word[:i] + "*" + word[i + 1:]) # O(m) to create string
-            return patterns
-        
-        wordList.append(beginWord)
+        patterns = defaultdict(list)
         
         graph = defaultdict(list)
         
-        for word in wordList: # n words
-            for pattern in get_patterns(word): # m patterns
+        if endWord not in wordList:
+            return 0
+        
+        wordList.append(beginWord)
+        
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i + 1:]
+                patterns[word].append(pattern)
                 graph[pattern].append(word)
         
         queue = deque([beginWord])
         visited = set([beginWord])
         level = 1
         
-        # 
         while queue:
             for i in range(len(queue)):
                 word = queue.popleft()
                 if word == endWord:
                     return level
-                patterns = get_patterns(word)
-                for pattern in patterns:
+                for pattern in patterns[word]:
                     for neighbor in graph[pattern]:
                         if neighbor not in visited and neighbor != word:
-                            visited.add(neighbor)
                             queue.append(neighbor)
+                            visited.add(neighbor)
             level += 1
+        
         return 0
-                
-            
