7d69c6a22832aa23feab2c76e12310f41350fa88
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-09 03:43:08 -0400

    Time: 61 ms (96.21%), Space: 15.1 MB (65.15%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        Dict = {}
+        seen = {}
         for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in Dict:
-                return [i, Dict[complement]]
-            Dict[nums[i]] = i
+            if target - nums[i] in seen:
+                return [i, seen[target - nums[i]]]
+            seen[nums[i]] = i
         return []
