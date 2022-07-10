f9c3856387de15b483d636b8d19bc6bc0f302d2f
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-10 03:19:32 -0400

    Time: 208 ms (70.32%), Space: 18.9 MB (12.63%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,35 +1,34 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        
-        patterns = defaultdict(list)
-        
-        graph = defaultdict(list)
-        
         if endWord not in wordList:
             return 0
+        
+        patterns_to_words = defaultdict(list)
+        words_to_patterns = defaultdict(list)
         
         wordList.append(beginWord)
         
         for word in wordList:
             for i in range(len(word)):
-                pattern = word[:i] + "*" + word[i + 1:]
-                patterns[word].append(pattern)
-                graph[pattern].append(word)
+                pattern = word[0 : i] + "*" + word[i+1:]
+                words_to_patterns[word].append(pattern)
+                patterns_to_words[pattern].append(word)
         
         queue = deque([beginWord])
         visited = set([beginWord])
         level = 1
         
         while queue:
-            for i in range(len(queue)):
+            for _ in range(len(queue)):
                 word = queue.popleft()
                 if word == endWord:
                     return level
-                for pattern in patterns[word]:
-                    for neighbor in graph[pattern]:
+                for pattern in words_to_patterns[word]:
+                    for neighbor in patterns_to_words[pattern]:
                         if neighbor not in visited and neighbor != word:
-                            queue.append(neighbor)
                             visited.add(neighbor)
+                            queue.append(neighbor)
             level += 1
         
         return 0
+        
