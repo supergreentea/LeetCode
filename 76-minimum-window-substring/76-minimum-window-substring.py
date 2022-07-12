9a797f099cfa5c3b0be3bcbdc8a55e65e5155a4c
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-12 02:38:12 -0400

    Time: 123 ms (78.58%), Space: 14.6 MB (82.88%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,39 +1,22 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t) or not s:
-            return ""
+        s_counter, t_counter = defaultdict(int), Counter(t)
         
-        s_counter, t_counter = defaultdict(int), defaultdict(int)
+        ans_l, ans_r = 0, len(s)
         
-        for c in t:
-            t_counter[c] += 1
-        
-        # values chosen so that if valid window substring is found with smaller length, we can update these values
-        # if ans_right remains len(s), then we know no valid substring window was found
-        ans_left, ans_right = 0, len(s)
-        
+        l = 0
         have, need = 0, len(t_counter)
         
-        window_left = 0
-        for window_right in range(len(s)):
-            cur_char = s[window_right]
-            if cur_char in t_counter:
-                s_counter[cur_char] += 1
-                if s_counter[cur_char] == t_counter[cur_char]:
-                    have += 1
-                    while have == need:
-                        if ans_right - ans_left > window_right - window_left:
-                            ans_left, ans_right = window_left, window_right
-                        front_char = s[window_left]
-                        s_counter[front_char] -= 1
-                        if front_char in t_counter and s_counter[front_char] == t_counter[front_char] - 1:
-                            have -= 1
-                        window_left += 1
-                        
-        if ans_right == len(s):
-            return ""
-        
-        return s[ans_left : ans_right + 1]
+        for r in range(len(s)):
+            s_counter[s[r]] += 1
+            if s_counter[s[r]] == t_counter[s[r]]:
+                have += 1
+                while have == need:
+                    if r - l < ans_r - ans_l:
+                        ans_l, ans_r = l, r
+                    s_counter[s[l]] -= 1
+                    if s_counter[s[l]] == t_counter[s[l]] - 1:
+                        have -= 1
+                    l += 1
                     
-        
-        
+        return s[ans_l : ans_r + 1] if ans_r != len(s) else ""
