b8f146ab97cbcb95f1c29047abdd618eedf14742
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-12 03:08:58 -0400

    Time: 148 ms (37.76%), Space: 14.1 MB (28.28%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,17 +2,16 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         output = []
         
-        def backtrack(index, comb, cur_sum):
-            if cur_sum == target:
+        def backtrack(index = 0, comb = [], comb_sum = 0):
+            if comb_sum == target:
                 output.append(comb.copy())
-            if cur_sum > target:
                 return
-            
+            if comb_sum > target:
+                return
             for i in range(index, len(candidates)):
                 comb.append(candidates[i])
-                backtrack(i, comb, cur_sum + candidates[i])
+                backtrack(i, comb, comb_sum + candidates[i])
                 comb.pop()
         
-        backtrack(0, [], 0)
-        
+        backtrack()
         return output
