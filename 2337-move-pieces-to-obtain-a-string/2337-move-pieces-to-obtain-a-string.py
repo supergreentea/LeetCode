a1c8ebf5d2f9fe7d6fb2fff28d0ef6fde311de47
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-12 01:26:17 -0400

    Time: 736 ms (22.10%), Space: 35.2 MB (12.90%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.py
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.py
@@ -1,19 +1,18 @@
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
+        if len(start) != len(target):
+            return False
+        
         start_pieces, target_pieces = [], []
-        blank_count_diff = 0
+
         for index, char in enumerate(start):
             if char != '_':
                 start_pieces.append((char, index))
-            else:
-                blank_count_diff += 1
         for index, char in enumerate(target):
             if char != '_':
                 target_pieces.append((char, index))
-            else:
-                blank_count_diff -=1
         
-        if blank_count_diff != 0 or len(start_pieces) != len(target_pieces):
+        if len(start_pieces) != len(target_pieces):
             return False
         
         for i in range(len(start_pieces)):
