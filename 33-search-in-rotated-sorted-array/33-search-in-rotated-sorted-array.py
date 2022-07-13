30c1844c9868bba4c1ec876153d8b67f981d4b0a
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-13 03:14:46 -0400

    Time: 57 ms (64.79%), Space: 14.2 MB (56.22%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,20 +1,35 @@
+"""
+Binary search.
+
+1. We have two pointers left and right starting at 0 and len(nums) - 1
+2. while left <= right:
+        get middle
+        if middle is target, return middle
+        if left half is sorted and target in left half, search left half else search right half
+        otherwise if right half is sorted and target in right half, search right half else search left half
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+"""
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-        
-        while l <= r:
-            m = (l + r) // 2
-            if nums[m] == target:
-                return m
-            if nums[m] >= nums[l]:
-                if target >= nums[l] and target < nums[m]:
-                    r = m - 1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            middle = left + (right - left) // 2
+            if nums[middle] == target:
+                return middle
+            
+            # left to middle is sorted
+            if nums[middle] >= nums[left]:
+                if target >= nums[left] and target < nums[middle]:
+                    right = middle - 1
                 else:
-                    l = m + 1
-            else:
-                if target > nums[m] and target <= nums[r]:
-                    l = m + 1
+                    left = middle + 1
+            else: # middle to right is sorted
+                if target > nums[middle] and target <= nums[right]:
+                    left = middle + 1
                 else:
-                    r = m - 1
-        
+                    right = middle - 1
         return -1
