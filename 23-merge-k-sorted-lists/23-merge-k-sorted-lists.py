1f1cd212634584fec311da5a185e07d7774650a4
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-13 16:24:09 -0400

    Time: 249 ms (20.43%), Space: 17.6 MB (73.76%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -8,25 +8,27 @@ class Solution:
         if not lists:
             return None
         
-        def merge(p, q):
-            cur = prehead = ListNode(0)
-            while p and q:
-                if p.val < q.val:
-                    cur.next = p
-                    p = p.next
+        def merge(list1, list2):
+            prehead = cur = ListNode()
+            while list1 and list2:
+                if list1.val < list2.val:
+                    cur.next = list1
+                    list1 = list1.next
                 else:
-                    cur.next = q
-                    q = q.next
+                    cur.next = list2
+                    list2 = list2.next
                 cur = cur.next
-            cur.next = p if p else q
+            cur.next = list1 if list1 else list2
+            
             return prehead.next
         
         while len(lists) > 1:
             merged_lists = []
             for i in range(0, len(lists), 2):
-                p = lists[i]
-                q = lists[i + 1] if i + 1 < len(lists) else None
-                merged_lists.append(merge(p, q))
+                list1 = lists[i]
+                list2 = lists[i + 1] if i + 1 < len(lists) else None
+                merged_lists.append(merge(list1, list2))
             lists = merged_lists
         
         return lists[0]
+            
