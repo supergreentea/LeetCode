0e2577a049d251924e46b5ba715740035febb436
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-14 00:39:55 -0400

    Time: 82 ms (14.35%), Space: 14 MB (36.34%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.py
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -1,30 +1,32 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         output = []
-
-        def valid(string):
-          if not string:
-            return False
-          if string[0] == "0":
-            return len(string) == 1
-          integer = int(string)
-          return integer >= 0 and integer <= 255
-
-        def backtrack(index = 0, address = [], dots_placed = 0):
-          if dots_placed == 3:
-            if valid(s[index:]):
-              address.append(s[index:])
-              output.append(".".join(address))
-              address.pop()
-              return
-
-          for i in range(1, 4):
-            if index + i < len(s):
-              substring = s[index : index + i]
-              if valid(substring):
-                address.append(substring)
-                backtrack(index + i, address, dots_placed + 1)
-                address.pop()
-
-        backtrack()
+        
+        if len(s) < 4:
+            return output
+        
+        def valid_substring(string):
+            if not string:
+                return False
+            if string[0] == '0':
+                return len(string) == 1
+            number = int(string)
+            return number > 0 and number <= 255
+        
+        def backtrack(index, builder, dots):
+            if dots == 3:
+                substring = s[index:]
+                if valid_substring(substring):
+                    builder.append(substring)
+                    output.append(".".join(builder))
+                    builder.pop()
+                    return
+            for i in range(1, 4):
+                substring = s[index : index + i]
+                if valid_substring(substring):
+                    builder.append(substring)
+                    backtrack(index + i, builder, dots + 1)
+                    builder.pop()
+        
+        backtrack(0, [], 0)
         return output
