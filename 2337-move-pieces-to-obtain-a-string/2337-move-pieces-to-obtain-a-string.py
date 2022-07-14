112659f2c5b5b14fc7e48f2e7ad1f644f44fc01a
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-14 00:52:11 -0400

    Time: 513 ms (50.98%), Space: 32.8 MB (14.55%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.py
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.py
@@ -1,32 +1,30 @@
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
+        start_pieces, target_pieces = [], []
+        
         if len(start) != len(target):
             return False
         
-        start_pieces, target_pieces = [], []
-
-        for index, char in enumerate(start):
-            if char != '_':
-                start_pieces.append((char, index))
-        for index, char in enumerate(target):
-            if char != '_':
-                target_pieces.append((char, index))
+        for i in range(len(start)):
+            if start[i] != '_':
+                start_pieces.append((start[i], i))
+            if target[i] != '_':
+                target_pieces.append((target[i], i))
         
         if len(start_pieces) != len(target_pieces):
             return False
         
         for i in range(len(start_pieces)):
-            start_char, start_index = start_pieces[i]
-            target_char, target_index = target_pieces[i]
-            if start_char == target_char:
-                if start_char == 'L':
-                    if start_index < target_index:
+            start_piece, start_piece_index = start_pieces[i]
+            target_piece, target_piece_index = target_pieces[i]
+            if start_piece != target_piece:
+                return False
+            else:
+                if start_piece == 'L':
+                    if start_piece_index < target_piece_index:
                         return False
                 else:
-                    if start_index > target_index:
+                    if start_piece_index > target_piece_index:
                         return False
-            else:
-                return False
         
         return True
-        
