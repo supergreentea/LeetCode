e355b54d5ca27616b18dd963b4c05b4017e210ce
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-15 01:23:01 -0400

    Time: 324 ms (80.88%), Space: 16.5 MB (13.37%) - LeetHub

--- 490-the-maze/490-the-maze.py
+++ 490-the-maze/490-the-maze.py
@@ -1,47 +1,25 @@
 class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
         visited = set()
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         ROWS, COLS = len(maze), len(maze[0])
         
-        def dfs(start_row, start_col):
-            if [start_row, start_col] == destination:
-                return True
+        def dfs(start_row = start[0], start_col = start[1]):
             if (start_row, start_col) in visited:
                 return False
+            if [start_row, start_col] == destination:
+                return True
             
             visited.add((start_row, start_col))
             
-            # go right
-            col = start_col
-            while col + 1 < COLS and maze[start_row][col + 1] != 1:
-                col += 1
-            if dfs(start_row, col):
-                return True
-            
-            # go left
-            col = start_col
-            while col - 1 >= 0 and maze[start_row][col - 1] != 1:
-                col -= 1
-            if dfs(start_row, col):
-                return True
-            
-            # go down
-            row = start_row
-            while row + 1 < ROWS and maze[row + 1][start_col] != 1:
-                row += 1
-            if dfs(row, start_col):
-                return True
-            
-            # go up
-            row = start_row
-            while row - 1 >= 0 and maze[row - 1][start_col] != 1:
-                row -= 1
-            if dfs(row, start_col):
-                return True
+            for d_row, d_col in directions:
+                row, col = start_row, start_col    
+                while row >= 0 and row < ROWS and col >= 0 and col < COLS and maze[row][col] != 1:
+                    row += d_row
+                    col += d_col
+                if dfs(row - d_row, col - d_col):
+                    return True
             
             return False
-                
-        return dfs(start[0], start[1])
-                
-            
-            
+        
+        return dfs()
