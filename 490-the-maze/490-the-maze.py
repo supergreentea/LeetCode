82fcb717d741b5918e61fc0f282089b252bc06c9
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-15 01:30:36 -0400

    Time: 622 ms (18.59%), Space: 14.7 MB (43.34%) - LeetHub

--- 490-the-maze/490-the-maze.py
+++ 490-the-maze/490-the-maze.py
@@ -1,22 +1,22 @@
 class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
-        visited = set()
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         ROWS, COLS = len(maze), len(maze[0])
         
-        def dfs(start_row = start[0], start_col = start[1]):
-            if (start_row, start_col) in visited:
-                return False
+        visited = set((start[0], start[1]))
+        queue = deque([(start[0], start[1])])
+        
+        while queue:
+            start_row, start_col = queue.popleft()
             if [start_row, start_col] == destination:
                 return True
-            visited.add((start_row, start_col))
             for d_row, d_col in directions:
-                row, col = start_row, start_col    
-                while row >= 0 and row < ROWS and col >= 0 and col < COLS and maze[row][col] != 1:
+                row, col = start_row, start_col
+                while row >= 0 and row < ROWS and col >= 0 and col < COLS and maze[row][col] == 0:
                     row += d_row
                     col += d_col
-                if dfs(row - d_row, col - d_col):
-                    return True
-            return False
+                if (row - d_row, col - d_col) not in visited:
+                    visited.add((row - d_row, col - d_col))
+                    queue.append((row - d_row, col - d_col))
         
-        return dfs()
+        return False
