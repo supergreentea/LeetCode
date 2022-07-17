e194ca59635dae7a0149b814802be59cb80df53a
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-16 21:18:46 -0400

    Time: 362 ms (53.43%), Space: 19 MB (12.43%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -1,18 +1,24 @@
 class Solution:
-
+    
+    # O(len(w))
     def __init__(self, w: List[int]):
         self.w = w
         self.prefix_sums = [0] * len(w)
         self.prefix_sums[0] = w[0]
         for i in range(1, len(w)):
             self.prefix_sums[i] = self.prefix_sums[i - 1] + w[i]
-
+    
+    # O(len(w))
     def pickIndex(self) -> int:
         random_number = randint(1, self.prefix_sums[-1])
-        for i in range(len(self.w)):
-            if random_number <= self.prefix_sums[i]:
-                return i
-
+        low, high = 0, len(self.w)
+        while low < high:
+            middle = low + (high - low) // 2
+            if random_number > self.prefix_sums[middle]:
+                low = middle + 1
+            else:
+                high = middle
+        return low
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
