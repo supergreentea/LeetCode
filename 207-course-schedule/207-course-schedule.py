f804ca447d940eb71fdc6439961158d3db9637e5
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-18 02:13:24 -0400

    Time: 177 ms (35.00%), Space: 15.5 MB (76.06%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,28 +1,33 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
         graph = defaultdict(list)
         for crs, pre in prerequisites:
             graph[crs].append(pre)
         
-        on_path, explored = set(), set()
-        
-        def dfs(crs):
-            if crs in on_path:
-                return False
-            if crs in explored:
-                return True
-            
-            on_path.add(crs)
-            for pre in graph[crs]:
-                if not dfs(pre):
-                    return False
-            on_path.remove(crs)
-            explored.add(crs)
-            return True
+        inDegree = [0 for crs in range(numCourses)]
         
         for crs in range(numCourses):
-            if not dfs(crs):
-                return False
+            for pre in graph[crs]:
+                inDegree[pre] += 1
+        
+        degZeroNodes = []
+        for crs in range(numCourses):
+            if inDegree[crs] == 0:
+                degZeroNodes.append(crs)
+        
+        topOrdering = []
+        
+        while degZeroNodes:
+            node = degZeroNodes.pop()
+            topOrdering.append(node)
+            for nbr in graph[node]:
+                inDegree[nbr] -= 1
+                if inDegree[nbr] == 0:
+                    degZeroNodes.append(nbr)
+        
+        if len(topOrdering) < numCourses:
+            return False
         
         return True
+        
+            
