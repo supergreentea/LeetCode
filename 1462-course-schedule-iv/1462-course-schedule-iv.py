7f92a3f4e25a893cb3d8f1d2e883dd31aec28e7c
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-19 03:31:31 -0400

    Time: 1057 ms (78.03%), Space: 17.6 MB (83.78%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        G = defaultdict(list)
+        indegree = defaultdict(int)
+        crs_to_prereqs = defaultdict(set)
+        
+        for pre, crs in prerequisites:
+            G[pre].append(crs)
+            crs_to_prereqs[crs].add(pre)
+            indegree[crs] += 1
+        
+        zero_indegree = [crs for crs in range(numCourses) if indegree[crs] == 0]
+        
+        top_ordering = []
+        
+        while zero_indegree:
+            pre = zero_indegree.pop()
+            top_ordering.append(pre)
+            for crs in G[pre]:
+                indegree[crs] -= 1
+                if indegree[crs] == 0:
+                    zero_indegree.append(crs)
+                crs_to_prereqs[crs].update(crs_to_prereqs[pre])
+        
+        answer = [(u in crs_to_prereqs[v]) for u, v in queries]
+        return answer
