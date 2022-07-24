1d15a5fb801abb2933e36e6c15cc7c9e1ee78ca8
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-24 19:15:24 -0400

    Time: 567 ms (69.14%), Space: 16.8 MB (25.53%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,25 +1,26 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        G = defaultdict(list)
+        graph = defaultdict(list)
         for u, v, w in times:
-            G[u].append((v, w))
+            graph[u].append((v, w))
         
-        min_times = { node : math.inf for node in range(1, n + 1) }
-        min_times[k] = 0
+        distance = { node : math.inf for node in range(1, n + 1) }
+        
+        distance[k] = 0
         
         PQ = [(0, k)]
         while PQ:
-            cur_time, cur_node = heappop(PQ)
-            if cur_time > min_times[cur_node]: continue
-            for nbr, w in G[cur_node]:
-                new_time = cur_time + w
-                if new_time < min_times[nbr]:
-                    min_times[nbr] = new_time
-                    heappush(PQ, (new_time, nbr))
+            cur_dist, cur_node = heappop(PQ)
+            if cur_dist > distance[cur_node]: continue
+            for nbr, w in graph[cur_node]:
+                new_dist = cur_dist + w
+                if new_dist < distance[nbr]:
+                    distance[nbr] = new_dist
+                    heappush(PQ, (new_dist, nbr))
         
         ans = 0
-        for time in min_times.values():
-            if time == math.inf:
+        for dist in distance.values():
+            if dist == math.inf:
                 return -1
-            ans = max(ans, time)
+            ans = max(dist, ans)
         return ans
