8fed83cd31681dfe6f01af7284b8a80ad34ce2a2
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-24 19:10:10 -0400

    Time: 665 ms (50.35%), Space: 16.8 MB (25.53%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,26 +1,25 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        
         G = defaultdict(list)
         for u, v, w in times:
             G[u].append((v, w))
         
-        dist = { node : math.inf for node in range(1, n + 1) }
-        dist[k] = 0
+        min_times = { node : math.inf for node in range(1, n + 1) }
+        min_times[k] = 0
+        
         PQ = [(0, k)]
         while PQ:
-            cur_d, node = heappop(PQ)
-            if cur_d > dist[node]:
-                continue
-            for nbr, w in G[node]:
-                new_d = cur_d + w
-                if new_d < dist[nbr]:
-                    dist[nbr] = new_d
-                    heappush(PQ, (new_d, nbr))
+            cur_time, cur_node = heappop(PQ)
+            if cur_time > min_times[cur_node]: continue
+            for nbr, w in G[cur_node]:
+                new_time = cur_time + w
+                if new_time < min_times[nbr]:
+                    min_times[nbr] = new_time
+                    heappush(PQ, (new_time, nbr))
         
         ans = 0
-        for d in dist.values():
-            if d == math.inf:
+        for time in min_times.values():
+            if time == math.inf:
                 return -1
-            ans = max(ans, d)
+            ans = max(ans, time)
         return ans
