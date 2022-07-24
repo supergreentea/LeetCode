a8f805b893e53a246b0ff0b2112542e5541555f3
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-23 22:11:11 -0400

    Time: 464 ms (97.15%), Space: 16.6 MB (33.37%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -6,21 +6,23 @@ class Solution:
 
         travel_time = [math.inf for _ in range(n + 1)]
         travel_time[k] = 0
-        max_travel_time = 0
-        visited = set()
 
         PQ = []
         heappush(PQ, (0, k))
         while PQ:
             delay, node = heappop(PQ)
-            visited.add(node)
+            if delay > travel_time[node]:
+                continue
             for neighbor, time in graph[node]:
-                if neighbor in visited:
-                    continue
                 new_travel_time = travel_time[node] + time
                 if new_travel_time < travel_time[neighbor]:
                     travel_time[neighbor] = new_travel_time
                     heappush(PQ, (new_travel_time, neighbor))
-                    max_travel_time = max(max_travel_time, new_travel_time)
 
-        return max(travel_time[1:]) if len(visited) == n else -1
+        max_travel_time = 0
+        for node in range(1, n + 1):
+            if travel_time[node] == math.inf:
+                return -1
+            max_travel_time = max(max_travel_time, travel_time[node])
+
+        return max_travel_time
