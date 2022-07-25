ac2c273543acd50ba913358e362a7b7b7888de20
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-24 23:58:48 -0400

    Time: 3015 ms (31.79%), Space: 17.1 MB (26.27%) - LeetHub

---
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maximumMinimumPath(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        scores = [[-math.inf for col in range(COLS)] for row in range(ROWS)]
+        scores[0][0] = grid[0][0]
+        visited = set([(0, 0)])
+        
+        def get_valid_neighbors(row, col):
+            valid_neighbors = []
+            for row_offset, col_offset in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                new_row, new_col = row + row_offset, col + col_offset
+                if new_row >= 0 and new_row < ROWS and new_col >= 0 and new_col < COLS and (new_row, new_col) not in visited:
+                    valid_neighbors.append((new_row, new_col))
+            return valid_neighbors
+        
+        PQ = [(-grid[0][0], 0, 0)]
+        while PQ:
+            negative_score, row, col = heappop(PQ)
+            score = -negative_score
+            if score < scores[row][col]:
+                continue
+            for neighbor_row, neighbor_col in get_valid_neighbors(row, col):
+                neighbor_val = grid[neighbor_row][neighbor_col]
+                new_score = min(score, neighbor_val)
+                if new_score > scores[neighbor_row][neighbor_col]:
+                    scores[neighbor_row][neighbor_col] = new_score
+                    heappush(PQ, (-new_score, neighbor_row, neighbor_col))
+                    visited.add((neighbor_row, neighbor_col))
+                    
+        return scores[ROWS - 1][COLS - 1]
+            
