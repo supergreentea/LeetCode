82ac03fb16b61111a78bb07b1a71561b0d178a2f
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-25 01:09:46 -0400

    Time: 398 ms, Memory: 15.5 MB - LeetHub

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -3,10 +3,11 @@ class Solution:
         l, r = 0, len(nums) - 1
         while l <= r:
             m = (l + r) // 2
-            if nums[m] < target:
-                l = m + 1
-            elif nums[m] > target:
-                r = m - 1
-            else:
+            if nums[m] == target:
                 return m
+            
+            if target < nums[m]:
+                r = m - 1
+            elif target > nums[m]:
+                l = m + 1
         return -1
