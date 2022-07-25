1caa9eba91a340f260fdfa1a0e09d691d670fc2c
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-25 01:19:43 -0400

    Time: 61 ms, Memory: 13.8 MB - LeetHub

--- sqrtx/sqrtx.py
+++ sqrtx/sqrtx.py
@@ -1,12 +1,25 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        l, r = 0, x
+        if x == 0:
+            return 0
+        if x <= 2:
+            return 1
+        
+        l, r = 1, x
+        
+        target = x
+        
         while l <= r:
-            m = l + (r - l) // 2
-            if m * m < x:
-                l = m + 1
-            elif m * m > x:
-                r = m - 1
-            else:
+            
+            m = (l + r) // 2
+            
+            square = m * m
+            if square == target:
                 return m
+            if target < square:
+                r = m - 1
+            elif target > square:
+                l = m + 1
+        
         return r
+            
