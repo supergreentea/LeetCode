05bc0e3952583386cd5341346c63c456536b8d8c
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-28 02:39:39 -0400

    Time: 256 ms (68.40%), Space: 18.7 MB (38.01%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,44 +1,37 @@
 class Solution:
     
-    def countLessEqual(self, matrix, mid, smaller, larger):
-        
-        count, n = 0, len(matrix)
-        row, col = n - 1, 0
-        
-        while row >= 0 and col < n:
-            if matrix[row][col] > mid:
-               
-                # As matrix[row][col] is bigger than the mid, let's keep track of the
-                # smallest number greater than the mid
-                larger = min(larger, matrix[row][col])
-                row -= 1
-                
-            else:
-                
-                # As matrix[row][col] is less than or equal to the mid, let's keep track of the
-                # biggest number less than or equal to the mid
-                
-                smaller = max(smaller, matrix[row][col])
-                count += row + 1
-                col += 1
-
-        return count, smaller, larger
+    
     
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        MIN, MAX = matrix[0][0], matrix[ROWS - 1][COLS - 1]
         
-        n = len(matrix)
-        start, end = matrix[0][0], matrix[n - 1][n - 1]
+        start, end = MIN, MAX
+        
+        def count_smaller(mid, smaller, larger):
+            count = 0
+            row, col = ROWS - 1, 0
+            
+            while row >= 0 and col < COLS:
+                if matrix[row][col] > mid:
+                    larger = min(larger, matrix[row][col])
+                    row -=1
+                else:
+                    smaller = max(smaller, matrix[row][col])
+                    col += 1
+                    count += row + 1
+                    
+            return count, smaller, larger
+        
         while start < end:
-            mid = start + (end - start) / 2
-            smaller, larger = (matrix[0][0], matrix[n - 1][n - 1])
-
-            count, smaller, larger = self.countLessEqual(matrix, mid, smaller, larger)
-
+            mid = (start + end) // 2
+            smaller, larger = MIN, MAX
+            count, smaller, larger = count_smaller(mid, smaller, larger)
             if count == k:
                 return smaller
             if count < k:
-                start = larger  # search higher
+                start = larger
             else:
-                end = smaller  # search lower
-
+                end = smaller
+        
         return start
