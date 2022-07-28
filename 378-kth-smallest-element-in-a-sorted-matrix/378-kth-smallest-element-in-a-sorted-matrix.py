589400a7cde5116f8c4f4a9c75f5a5dfff3a3a1b
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-28 18:42:32 -0400

    Time: 185 ms (95.32%), Space: 18.7 MB (38.01%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -2,9 +2,10 @@ class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         ROWS, COLS = len(matrix), len(matrix[0])
         MIN, MAX = matrix[0][0], matrix[ROWS - 1][COLS - 1]
+        
         start, end = MIN, MAX
         
-        def get_counts(mid, smaller, larger):
+        def get_count(mid, smaller, larger):
             count = 0
             row, col = ROWS - 1, 0
             while row >= 0 and col < COLS:
@@ -17,15 +18,16 @@ class Solution:
                     col += 1
                     count += row + 1
             return count, smaller, larger
-                    
+        
         while start < end:
             mid = (start + end) // 2
             smaller, larger = MIN, MAX
-            count, smaller, larger = get_counts(mid, smaller, larger)
+            count, smaller, larger = get_count(mid, smaller, larger)
             if count == k:
                 return smaller
             if count < k:
                 start = larger
             else:
                 end = smaller
+        
         return start
