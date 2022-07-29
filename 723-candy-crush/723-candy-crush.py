cf9415d6490821908f20cab5fa5595d6ee04d081
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-29 01:02:27 -0400

    Time: 335 ms (31.82%), Space: 14 MB (49.18%) - LeetHub

--- 723-candy-crush/723-candy-crush.py
+++ 723-candy-crush/723-candy-crush.py
@@ -1,33 +1,32 @@
 class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
-        
         ROWS, COLS = len(board), len(board[0])
         
-        needs_crushing = False
+        todo = False
         
-        # mark horizontally
+        # mark horizontally candies
         for row in range(ROWS):
             for col in range(COLS - 2):
                 if abs(board[row][col]) == abs(board[row][col + 1]) == abs(board[row][col + 2]) != 0:
                     board[row][col] = board[row][col + 1] = board[row][col + 2] = -abs(board[row][col])
-                    needs_crushing = True
+                    todo = True
         
-        # mark vertically
+        # mark vertical candies
         for row in range(ROWS - 2):
             for col in range(COLS):
                 if abs(board[row][col]) == abs(board[row + 1][col]) == abs(board[row + 2][col]) != 0:
                     board[row][col] = board[row + 1][col] = board[row + 2][col] = -abs(board[row][col])
-                    needs_crushing = True
+                    todo = True
         
-        # gravity step
+        # crush candies
         for col in range(COLS):
             write_head = ROWS - 1
             for row in range(ROWS - 1, -1, -1):
                 if board[row][col] > 0:
                     board[write_head][col] = board[row][col]
                     write_head -= 1
+            
             for write_head in range(write_head, -1, -1):
                 board[write_head][col] = 0
         
-        return self.candyCrush(board) if needs_crushing else board
-            
+        return self.candyCrush(board) if todo else board
