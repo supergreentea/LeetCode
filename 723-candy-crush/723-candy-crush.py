f105d15c1cc2a1e4ef3d9742feb5e19c155fac5b
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-29 00:50:21 -0400

    Time: 233 ms (73.84%), Space: 14.1 MB (49.18%) - LeetHub

--- 723-candy-crush/723-candy-crush.py
+++ 723-candy-crush/723-candy-crush.py
@@ -1,35 +1,33 @@
 class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
+        
         ROWS, COLS = len(board), len(board[0])
         
-        crush = False
+        needs_crushing = False
         
-        # check horizontal windows of length 3 and mark for crushing
+        # mark horizontally
         for row in range(ROWS):
             for col in range(COLS - 2):
                 if abs(board[row][col]) == abs(board[row][col + 1]) == abs(board[row][col + 2]) != 0:
                     board[row][col] = board[row][col + 1] = board[row][col + 2] = -abs(board[row][col])
-                    crush = True
+                    needs_crushing = True
         
-        # check vertical windows of length 3 and mark for crushing
-        for col in range(COLS):
-            for row in range(ROWS - 2):
+        # mark vertically
+        for row in range(ROWS - 2):
+            for col in range(COLS):
                 if abs(board[row][col]) == abs(board[row + 1][col]) == abs(board[row + 2][col]) != 0:
                     board[row][col] = board[row + 1][col] = board[row + 2][col] = -abs(board[row][col])
-                    crush = True
+                    needs_crushing = True
         
-        # gravity
+        # gravity step
         for col in range(COLS):
             write_head = ROWS - 1
-            for row in range(ROWS - 1, -1 , -1):
+            for row in range(ROWS - 1, -1, -1):
                 if board[row][col] > 0:
                     board[write_head][col] = board[row][col]
                     write_head -= 1
             for write_head in range(write_head, -1, -1):
                 board[write_head][col] = 0
         
-        if not crush:
-            return board
-        
-        return self.candyCrush(board)
-        
+        return self.candyCrush(board) if needs_crushing else board
+            
