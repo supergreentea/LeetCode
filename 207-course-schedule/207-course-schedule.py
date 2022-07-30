197a24bc20a3e53748f606f7252a3832026564f8
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-30 17:22:01 -0400

    Time: 110 ms (86.06%), Space: 15.4 MB (89.31%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,7 +1,7 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        # build adjacency list
+        # build gragh
         G = defaultdict(list)
         indegree = defaultdict(int)
         
@@ -9,25 +9,18 @@ class Solution:
             G[pre].append(crs)
             indegree[crs] += 1
         
-        # collect courses with zero prerequisites
         zero_indegree = []
         for crs in range(numCourses):
             if indegree[crs] == 0:
                 zero_indegree.append(crs)
         
         top_ordering = []
-        
         while zero_indegree:
             pre = zero_indegree.pop()
             top_ordering.append(pre)
-            
-            # decrease degree for dependencies
             for crs in G[pre]:
                 indegree[crs] -= 1
                 if indegree[crs] == 0:
                     zero_indegree.append(crs)
         
-        # if we are not able to get all courses into the topological ordering then there was a cycle
-        return len(top_ordering) == numCourses
-        
-        
+        return top_ordering if len(top_ordering) == numCourses else []
