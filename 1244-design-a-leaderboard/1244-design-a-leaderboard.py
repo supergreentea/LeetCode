4957c8c79668889b0571cc8e961a26193085ea0d
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-07-30 00:09:07 -0400

    Time: 109 ms (60.37%), Space: 14.7 MB (13.21%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -1,24 +1,47 @@
+from sortedcontainers import SortedDict
+
 class Leaderboard:
 
     def __init__(self):
-        self.player_scores = defaultdict(int)
+        self.scores = {}
+        self.sorted_scores = SortedDict()
+        
 
     def addScore(self, playerId: int, score: int) -> None:
-        self.player_scores[playerId] += score
+        if playerId not in self.scores:
+            self.scores[playerId] = score
+            self.sorted_scores[-score] = self.sorted_scores.get(-score, 0) + 1
+        else:
+            old_score = self.scores[playerId]
+            new_score = old_score + score
+            self.scores[playerId] = new_score
+            
+            self.sorted_scores[-old_score] -= 1
+            if self.sorted_scores[-old_score] == 0:
+                del self.sorted_scores[-old_score]
+            
+            self.sorted_scores[-new_score] = self.sorted_scores.get(-new_score, 0) + 1
 
     def top(self, K: int) -> int:
-        heap = []
-        for score in self.player_scores.values():
-            heappush(heap, score)
-            if len(heap) > K:
-                heappop(heap)
-        score_sum = 0
-        while heap:
-            score_sum += heappop(heap)
-        return score_sum
+        count = total = 0
+        for score, score_count in self.sorted_scores.items():
+            for _ in range(score_count):
+                total += -score
+                count += 1
+                if count == K:
+                    break
+            if count == K:
+                break
+        return total
+        
 
     def reset(self, playerId: int) -> None:
-        self.player_scores[playerId] = 0
+        score = self.scores[playerId]
+        del self.scores[playerId]
+        
+        self.sorted_scores[-score] -= 1
+        if self.sorted_scores[-score] == 0:
+            del self.sorted_scores[-score]
 
 
 # Your Leaderboard object will be instantiated and called as such:
