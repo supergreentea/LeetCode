6951f23363c30eca4bf66ff5b1aa0b58ddd08c60
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-08-02 23:38:44 -0400

    Time: 429 ms (92.87%), Space: 61.7 MB (6.79%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -1,30 +1,32 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.list = []
-        self.map = {}
-        
+        self.item_to_index = {}
+        self.items_list = []
 
     def insert(self, val: int) -> bool:
-        not_present = not val in self.map
-        if not_present:
-            self.map[val] = len(self.list)
-            self.list.append(val)
-        return not_present
+        if val not in self.item_to_index:
+            self.item_to_index[val] = len(self.items_list)
+            self.items_list.append(val)
+            return True
+        return False
 
     def remove(self, val: int) -> bool:
-        present = val in self.map
-        if present:
-            val_index = self.map[val]
-            last_val = self.list[-1]
-            self.list[val_index] = last_val
-            self.list.pop()
-            self.map[last_val] = val_index
-            del self.map[val]
-        return present
+        if val in self.item_to_index:
+            val_index = self.item_to_index[val]
+            last_val = self.items_list[-1]
+            self.items_list[val_index] = last_val
+            self.items_list.pop()
+            self.item_to_index[last_val] = val_index
+            del self.item_to_index[val]
+            return True
+        
+        return False
+            
 
     def getRandom(self) -> int:
-        return self.list[randint(0, len(self.list) - 1)]
+        return random.choice(self.items_list)
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
