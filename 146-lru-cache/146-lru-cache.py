4df339c062a290b2a6e71f66c4c8d71c2f6f5004
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-08-03 00:44:01 -0400

    Time: 1993 ms (10.93%), Space: 76.5 MB (24.62%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -10,28 +10,26 @@ class LRUCache:
         self.cache = {}
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
-        
         self.capacity = capacity
     
-    # remove node from linked list
+    # insert at end
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+        
+    # remove node
     def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
-        
-    # insert node to right of linked list
-    def insert(self, node):
-        prev = self.right.prev
-        prev.next = self.right.prev = node
-        node.prev, node.next = prev, self.right
-        
-        
+    
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-        
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
