13d4a99ca10092173ab75bc35aa51875539fcbb2
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-08-08 20:29:42 -0400

    Time: 59 ms (56.71%), Space: 14.1 MB (26.18%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,14 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        @cache
-        def backtrack(index = 0):
-            if index == len(s):
+        wordSet = set(wordDict)
+        
+        @lru_cache
+        def dfs(start = 0):
+            if start == len(s):
                 return True
-            for i in range(index, len(s)):
-                if i + 1 <= len(s) and s[index : i + 1] in wordDict:
-                    if backtrack(i + 1):
-                        return True
+            for end in range(start + 1, len(s) + 1):
+                if s[start : end] in wordSet and dfs(end):
+                    return True
             return False
         
-        return backtrack()
+        return dfs()
+            
