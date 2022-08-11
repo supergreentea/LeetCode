7e53060482661f64fee1e30a6e39fd428eb8d7a2
supergreentea <45723762+supergreentea@users.noreply.github.com>
2022-08-11 01:33:03 -0400

    Time: 220 ms (73.98%), Space: 15.6 MB (65.82%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,12 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        slow = 0
-        for fast, num in enumerate(nums):
-            if num != 0:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
-                slow += 1
-                
-        
-        
-            
+        write_head = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[write_head] = nums[i]
+                write_head += 1
+        for i in range(write_head, len(nums)):
+            nums[i] = 0
