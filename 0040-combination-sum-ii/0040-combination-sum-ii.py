3003d8c3d859e064a97feeaeff3a23afc2c3a363
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-14 01:35:31 -0400

    Time: 55 ms (75.99%), Space: 16.7 MB (41.64%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,16 +1,16 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
+        candidates.sort() # help us manage duplicates
         combinations = []
         
-        def backtrack(index: int = 0, combination: List[int] = [], currentSum: int = 0):
+        def backtrack(index: int = 0, combination: List[int] = [], currentSum = 0):
             if currentSum > target:
-                return
+                return 
             if currentSum == target:
                 combinations.append(combination[:])
                 return
             for i in range(index, len(candidates)):
-                if i > index and candidates[i] == candidates[i - 1]:
+                if i > index and candidates[i] == candidates[i - 1]: # avoid duplicate combinations
                     continue
                 combination.append(candidates[i])
                 backtrack(i + 1, combination, currentSum + candidates[i])
@@ -18,4 +18,3 @@ class Solution:
         
         backtrack()
         return combinations
-        
