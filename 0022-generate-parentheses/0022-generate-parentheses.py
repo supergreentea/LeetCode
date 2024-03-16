0c6744176159ae084b4ef18e9af4cec90794e860
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-16 02:01:29 -0400

    Time: 40 ms (42.18%), Space: 16.9 MB (21.17%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,18 +2,18 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         answer = []
         
-        def backtrack(cur: [], leftCount: int, rightCount: int):
-            if len(cur) ==  2 * n:
-                answer.append("".join(cur))
+        def backtrack(combination: List[int], left: int, right: int):
+            if len(combination) == 2 * n:
+                answer.append("".join(combination))
                 return
-            if leftCount < n:
-                cur.append("(")
-                backtrack(cur, leftCount + 1, rightCount)
-                cur.pop()
-            if rightCount < leftCount:
-                cur.append(")")
-                backtrack(cur, leftCount, rightCount + 1)
-                cur.pop()
+            if left < n:
+                combination.append("(")
+                backtrack(combination, left + 1, right)
+                combination.pop()
+            if right < left:
+                combination.append(")")
+                backtrack(combination, left, right + 1)
+                combination.pop()
         
         backtrack([], 0, 0)
         return answer
