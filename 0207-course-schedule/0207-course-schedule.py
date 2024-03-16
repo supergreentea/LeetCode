1b705320290957b5ff86a06e864d1b81bed289ea
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-16 02:16:04 -0400

    Time: 91 ms (52.89%), Space: 18.5 MB (24.66%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         G = defaultdict(list)
-        onStack = [False] * numCourses
-        visited = [False] * numCourses
+        indegrees = defaultdict(int)
+        for crs, pre in prerequisites:
+            G[pre].append(crs)
+            indegrees[crs] += 1
         
-        for course, prerequisite in prerequisites:
-            G[prerequisite].append(course)
-        
-        def isCyclic(course: int):
-            if onStack[course]:
-                return True
-            if visited[course]:
-                return False
-            onStack[course] = True
-            visited[course] = True
-            for neighbor in G[course]:
-                if isCyclic(neighbor):
-                    return True
-            onStack[course] = False
-            return False
-        
-        for course in range(numCourses):
-            if isCyclic(course):
-                return False
-        
-        return True
+        zeroIndegrees = []
+        for crs in range(numCourses):
+            if indegrees[crs] == 0:
+                zeroIndegrees.append(crs)
+        topologicalOrdering = []
+        while zeroIndegrees:
+            pre = zeroIndegrees.pop()
+            topologicalOrdering.append(pre)
+            for neighbor in G[pre]:
+                indegrees[neighbor] -= 1
+                if indegrees[neighbor] == 0:
+                    zeroIndegrees.append(neighbor)
+        return len(topologicalOrdering) == numCourses
+                    
+                    
