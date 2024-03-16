18840b4e4f341b6e784ad85fef0a42d69ecb2fcd
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-16 02:07:53 -0400

    Time: 43 ms (14.64%), Space: 16.4 MB (92.53%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -8,14 +8,15 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        rightside = []
+        
+        answer = []
         
         def helper(node: TreeNode, level: int) -> None:
-            if level == len(rightside):
-                rightside.append(node.val)
+            if level == len(answer):
+                answer.append(node.val)
             for child in [node.right, node.left]:
                 if child:
                     helper(child, level + 1)
         
         helper(root, 0)
-        return rightside
+        return answer
