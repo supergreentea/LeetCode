4e4ec98cbe16ac368613e817920bac93e7b94043
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-18 17:05:39 -0400

    Time: 4533 ms (38.53%), Space: 16.6 MB (71.94%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,23 +3,27 @@ class Solution:
         ROWS, COLS = len(board), len(board[0])
         visited = set()
         
-        def backtrack(index: int, row: int, col: int) -> None:
+        wordIndex = 0
+        
+        def searchWord(index: int, row: int, col: int) -> bool:
             if index == len(word):
                 return True
-            if (row < 0 or row >= ROWS or col < 0 or col >= COLS) or \
-                (row, col) in visited or word[index] != board[row][col]:
+            
+            if (row < 0 or row >= ROWS) or (col < 0 or col >= COLS) or \
+                board[row][col] != word[index] or (row, col) in visited:
                 return False
+            
             visited.add((row, col))
             for rowOffset, colOffset in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
                 newRow, newCol = row + rowOffset, col + colOffset
-                if backtrack(index + 1, newRow, newCol):
+                if searchWord(index + 1, newRow, newCol):
                     return True
             visited.remove((row, col))
             return False
         
         for row in range(ROWS):
             for col in range(COLS):
-                if backtrack(0, row, col):
+                if searchWord(0, row, col):
                     return True
         
         return False
