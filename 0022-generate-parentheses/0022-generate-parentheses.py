13eec954ca7b27fa6a484e533550f5970ec25cc7
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-20 02:54:42 -0400

    Time: 34 ms (80.07%), Space: 16.7 MB (89.76%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,9 +2,9 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         answer = []
         
-        def backtrack(combination: List[int], left: int, right: int):
+        def backtrack(combination: List[str], left: int, right: int) -> None:
             if len(combination) == 2 * n:
-                answer.append("".join(combination))
+                answer.append("".join(combination[:]))
                 return
             if left < n:
                 combination.append("(")
@@ -16,4 +16,5 @@ class Solution:
                 combination.pop()
         
         backtrack([], 0, 0)
+        
         return answer
