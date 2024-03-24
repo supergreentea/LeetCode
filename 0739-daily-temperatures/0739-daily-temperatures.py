9d8a9c8d0ec471d3f759b5493c53fc3ff4be0ec0
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-24 00:32:10 -0400

    Time: 865 ms (90.09%), Space: 30.1 MB (84.51%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,13 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         N = len(temperatures)
-        answers = [0] * N
+        daysUntilWarmer = [0] * N
+        
         stack = []
         
-        for day, temp in enumerate(temperatures):
-            while stack and temperatures[stack[-1]] < temp:
+        for day, temperature in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temperature:
                 prevDay = stack.pop()
-                answers[prevDay] = day - prevDay
+                daysUntilWarmer[prevDay] = day - prevDay
             stack.append(day)
         
-        return answers
+        return daysUntilWarmer
