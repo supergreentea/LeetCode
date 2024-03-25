0ec5fdecbf5aaf9c2503ed72f90626690d285b01
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-25 02:57:18 -0400

    Time: 5809 ms (17.38%), Space: 16.5 MB (71.74%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,29 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
+        index = 0
         visited = set()
+        N = len(word)
         
-        wordIndex = 0
+        def withinBounds(row: int, col: int) -> bool:
+            return row >= 0 and row < ROWS and col >= 0 and col < COLS
         
-        def searchWord(index: int, row: int, col: int) -> bool:
-            if index == len(word):
+        def search(index: int, row: int, col: int) -> bool:
+            if index == N:
                 return True
             
-            if (row < 0 or row >= ROWS) or (col < 0 or col >= COLS) or \
-                board[row][col] != word[index] or (row, col) in visited:
+            if not withinBounds(row, col) or (row, col) in visited or board[row][col] != word[index]:
                 return False
             
             visited.add((row, col))
-            for rowOffset, colOffset in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
+            for rowOffset, colOffset in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
                 newRow, newCol = row + rowOffset, col + colOffset
-                if searchWord(index + 1, newRow, newCol):
+                if search(index + 1, newRow, newCol):
                     return True
             visited.remove((row, col))
             return False
         
         for row in range(ROWS):
             for col in range(COLS):
-                if searchWord(0, row, col):
+                if search(0, row, col):
                     return True
         
         return False
+            
