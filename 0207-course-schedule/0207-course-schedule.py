0e30cf81c214c3e6b7d661f545da6813013dc3ed
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-28 01:47:12 -0400

    Time: 84 ms (85.85%), Space: 18.3 MB (53.58%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,23 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        G = defaultdict(list)
+        graph = defaultdict(list)
+        
         indegrees = defaultdict(int)
+        
         for crs, pre in prerequisites:
-            G[pre].append(crs)
+            graph[pre].append(crs)
             indegrees[crs] += 1
         
         zeroIndegrees = []
+        
         for crs in range(numCourses):
             if indegrees[crs] == 0:
                 zeroIndegrees.append(crs)
+        
         topologicalOrdering = []
         while zeroIndegrees:
-            pre = zeroIndegrees.pop()
-            topologicalOrdering.append(pre)
-            for neighbor in G[pre]:
+            crs = zeroIndegrees.pop()
+            topologicalOrdering.append(crs)
+            for neighbor in graph[crs]:
                 indegrees[neighbor] -= 1
                 if indegrees[neighbor] == 0:
                     zeroIndegrees.append(neighbor)
         return len(topologicalOrdering) == numCourses
-                    
-                    
