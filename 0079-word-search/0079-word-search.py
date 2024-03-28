661a02d8869d2a41f7f15c4d727a05a2012ea677
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-28 01:09:21 -0400

    Time: 5398 ms (22.51%), Space: 16.5 MB (71.77%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,30 +3,29 @@ class Solution:
         ROWS, COLS = len(board), len(board[0])
         index = 0
         visited = set()
-        N = len(word)
         
         def withinBounds(row: int, col: int) -> bool:
             return row >= 0 and row < ROWS and col >= 0 and col < COLS
         
-        def search(index: int, row: int, col: int) -> bool:
-            if index == N:
+        
+        def backtrack(index: int, row: int, col: int) -> bool:
+            if index >= len(word):
                 return True
             
-            if not withinBounds(row, col) or (row, col) in visited or board[row][col] != word[index]:
+            if (row, col) in visited or not withinBounds(row, col) \
+                or word[index] != board[row][col]:
                 return False
-            
             visited.add((row, col))
-            for rowOffset, colOffset in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+            for rowOffset, colOffset in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 newRow, newCol = row + rowOffset, col + colOffset
-                if search(index + 1, newRow, newCol):
+                if backtrack(index + 1, newRow, newCol):
                     return True
             visited.remove((row, col))
             return False
         
         for row in range(ROWS):
             for col in range(COLS):
-                if search(0, row, col):
+                if backtrack(0, row, col):
                     return True
         
         return False
-            
