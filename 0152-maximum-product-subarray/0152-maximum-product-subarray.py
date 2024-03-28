6db8e78795cb26ae786511a1e79becd6a4dcc128
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-28 02:03:12 -0400

    Time: 75 ms (39.30%), Space: 17 MB (51.70%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -3,15 +3,13 @@ class Solution:
         N = len(nums)
         if N == 0:
             return 0
-        
-        maxSoFar = nums[0]
-        minSoFar = nums[0]
+        minSoFar = maxSoFar = result = nums[0]
         result = maxSoFar
-        
         for i in range(1, N):
-            curr = nums[i]
-            tempMax = max(curr, maxSoFar * curr, minSoFar * curr)
-            minSoFar = min(curr, maxSoFar * curr, minSoFar * curr)
-            maxSoFar = tempMax
-            result = max(maxSoFar, result)
+            num = nums[i]
+            newMax = max(num, maxSoFar * num, minSoFar * num)
+            minSoFar = min(num, maxSoFar * num, minSoFar * num)
+            maxSoFar = newMax
+            result = max(result, maxSoFar)
         return result
+            
