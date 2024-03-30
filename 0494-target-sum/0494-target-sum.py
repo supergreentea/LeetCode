2e49cd73849333fe35b9a9f018b764463ec7c84e
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-30 16:47:12 -0400

    Time: 183 ms (71.58%), Space: 43.8 MB (10.41%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,14 +1,12 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        #time: O(N * sum of nums)
-        #space: O(N) -> max depth of recursion
         
         @cache
-        def getCount(index: int, currentSum: int) -> int:
-            if index >= len(nums):
+        def getCount(index: int = 0, currentSum: int = 0) -> int:
+            if index == len(nums):
                 if currentSum == target:
                     return 1
                 return 0
             return getCount(index + 1, currentSum + nums[index]) + getCount(index + 1, currentSum - nums[index])
         
-        return getCount(0, 0)
+        return getCount()
