708df15f410a2cc7b39d395e8da7854caded51de
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-03-29 23:02:07 -0400

    Time: 638 ms (53.36%), Space: 157.2 MB (11.62%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,20 +1,18 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        N = len(nums)
-        
         total = sum(nums)
         if total % 2 == 1:
             return False
         
         @cache
         def dp(index: int = 0, currentSum: int = 0) -> bool:
-            if index >= len(nums):
+            if index == len(nums):
                 return False
             remainingSum = total - currentSum
-            if currentSum > remainingSum: 
+            if currentSum > remainingSum:
                 return False
             if currentSum == remainingSum:
                 return True
-            return dp(index + 1, currentSum) or dp(index + 1, currentSum + nums[index])
+            return dp(index + 1, currentSum + nums[index]) or dp(index + 1, currentSum)
         
         return dp()
