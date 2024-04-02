569ea3cc590c18fa357c8e56ae053c9f24661624
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-02 16:29:48 -0400

    Time: 1637 ms (23.63%), Space: 499.3 MB (5.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,21 +1,19 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        if len(nums) < 2:
+            return False
         
-        # O(m * n) for both time and space
-        # m is subset sum and n is number of elements in array
         total = sum(nums)
         if total % 2 == 1:
             return False
         
         @cache
-        def dp(index: int = 0, currentSum: int = 0) -> bool:
-            if index == len(nums):
+        def partition(index: int = 0, currentSum: int = 0) -> bool:
+            if index >= len(nums):
                 return False
             remainingSum = total - currentSum
-            if currentSum > remainingSum:
-                return False
             if currentSum == remainingSum:
                 return True
-            return dp(index + 1, currentSum + nums[index]) or dp(index + 1, currentSum)
+            return partition(index + 1, currentSum + nums[index]) or partition(index + 1, currentSum)
         
-        return dp()
+        return partition()
