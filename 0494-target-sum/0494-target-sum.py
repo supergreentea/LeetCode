f5dbd04725a4a0d47c3d79cc2f6f68bda7e916ec
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-03 01:42:54 -0400

    Time: 191 ms (67.50%), Space: 44.2 MB (7.80%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,12 +1,17 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        count = 0
         
         @cache
-        def getCount(index: int = 0, currentSum: int = 0) -> int:
+        def dp(index: int = 0, currentSum: int = 0) -> int:
+            nonlocal count
             if index == len(nums):
                 if currentSum == target:
                     return 1
                 return 0
-            return getCount(index + 1, currentSum + nums[index]) + getCount(index + 1, currentSum - nums[index])
+            add = dp(index + 1, currentSum + nums[index])
+            subtract = dp(index + 1, currentSum - nums[index])
+            return add +  subtract
         
-        return getCount()
+        return dp()
+            
