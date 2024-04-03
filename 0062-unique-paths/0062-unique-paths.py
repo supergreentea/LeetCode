1db2f93afea8458cdc3f6aa6af6feb8a033f79c4
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-03 02:07:20 -0400

    Time: 34 ms (69.83%), Space: 16.5 MB (86.83%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -3,5 +3,6 @@ class Solution:
         dp = [[1] * n for _ in range(m)]
         for row in range(1, m):
             for col in range(1, n):
-                dp[row][col] = dp[row-1][col] + dp[row][col-1]
-        return dp[m-1][n-1]
+                dp[row][col] = dp[row - 1][col] + dp[row][col - 1]
+        return dp[m - 1][n - 1]
+                                            
