7a5fda4bfb83195b6a60b41f000298f156c1dc25
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-04 04:28:12 -0400

    Time: 165 ms (5.09%), Space: 17.4 MB (28.67%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -4,18 +4,18 @@ class Solution:
             return False
         
         @cache
-        def dp(s1_index: int, s2_index: int) -> bool:
+        def dp(s1_index: int = 0, s2_index: int = 0) -> bool:
             s3_index = s1_index + s2_index
             if s3_index == len(s3):
                 return True
             for i in range(s1_index, len(s1)):
-                if s1[s1_index : i + 1] == s3[s3_index: s3_index +  (i - s1_index) + 1]:
+                if s1[s1_index: i + 1] == s3[s3_index: s3_index + (i - s1_index) + 1]:
                     if dp(i + 1, s2_index):
                         return True
             for i in range(s2_index, len(s2)):
-                if s2[s2_index : i + 1] == s3[s3_index: s3_index +  (i - s2_index) + 1]:
+                if s2[s2_index: i + 1] == s3[s3_index: s3_index + (i - s2_index) + 1]:
                     if dp(s1_index, i + 1):
                         return True
             return False
         
-        return dp(0, 0)
+        return dp()
