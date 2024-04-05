47f683a3d613f1df20569dedf79836dcdc7220f3
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-05 03:07:16 -0400

    Time: 43 ms (67.61%), Space: 20.5 MB (10.37%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,15 +2,14 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
         @cache
-        def maxProfit(index: int = 0, buy: bool = True) -> int:
-            if index >= len(prices):
+        def profit(day: int = 0, buying: bool = True) -> int:
+            if day >= len(prices):
                 return 0
-            
-            cooldown = maxProfit(index + 1, buy)
-            if buy:
-                buy = maxProfit(index + 1, not buy) - prices[index]
-                return max(buy, cooldown)
-            sell = maxProfit(index + 2, not buy) + prices[index]
-            return max(sell, cooldown)
+            skip = profit(day + 1, buying)
+            if buying:
+                buy = profit(day + 1, not buying) - prices[day]
+                return max(buy, skip)
+            sell = profit(day + 2, not buying) + prices[day]
+            return max(skip, sell)
         
-        return maxProfit()
+        return profit()
