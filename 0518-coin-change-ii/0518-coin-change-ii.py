15d5baf53bf0d2f08d86c7744faf02cd45c9593c
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-06 02:48:55 -0400

    Time: 313 ms (37.38%), Space: 34.8 MB (38.45%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,15 +1,25 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
-        @cache
-        def dp(index: int = 0, currentAmount: int = 0) -> int:
-            if index >= len(coins):
-                return 0
-            if currentAmount > amount:
-                return 0
-            if currentAmount == amount:
-                return 1
-            return dp(index, currentAmount + coins[index]) + dp(index + 1, currentAmount)
+        memo = [[-1] * (amount + 1) for _ in range(len(coins))]
         
-        return dp()
+        def numberOfWays(i: int, amount: int) -> int:
+            if amount == 0:
+                return 1
+
+            if i == len(coins):
+                return 0
+
+            if memo[i][amount] != -1:
+                return memo[i][amount]
             
+            if coins[i] > amount:
+                 memo[i][amount] = numberOfWays(i + 1, amount)
+            else:
+                memo[i][amount] = numberOfWays(i, amount - coins[i]) + numberOfWays(i + 1, amount)
+            
+            return memo[i][amount]
+
+        return numberOfWays(0, amount)
+        
+        
