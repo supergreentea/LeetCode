199fe1a40034d922a8fa6b4a00a1775773770104
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-05 23:35:22 -0400

    Time: 166 ms (39.53%), Space: 16.5 MB (57.40%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,18 +1,16 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # time: O(N ^ 2)
-        # space: O(1)
-        
         count = 0
         
-        def expandAroundCenter(left: int, right: int) -> None:
+        def expand(left: int, right: int) -> None:
             nonlocal count
             while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
                 count += 1
-                left, right = left - 1, right + 1
         
         for i in range(len(s)):
-            expandAroundCenter(i, i)
-            expandAroundCenter(i, i + 1)
+            expand(i, i)
+            expand(i, i + 1)
         
         return count
