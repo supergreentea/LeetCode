2755bf31b7115d140bce276bc4554f4593f7e09b
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-09 01:10:41 -0400

    Time: 1125 ms (15.42%), Space: 310.6 MB (6.45%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,19 +1,12 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        M, N = len(text1), len(text2)
-        
-        # there are M * N subproblems (M * N possible pairs of integers... M for first string and N for second string)
         
         @cache
         def dp(index1: int = 0, index2: int = 0) -> int:
-            if index1 == M or index2 == N:
+            if index1 == len(text1) or index2 == len(text2):
                 return 0
-            
-            # if the first character of each string is the same
             if text1[index1] == text2[index2]:
                 return 1 + dp(index1 + 1, index2 + 1)
-            
-            # not the same, so one or both chracters will not be used
             return max(dp(index1 + 1, index2), dp(index1, index2 + 1))
         
         return dp()
