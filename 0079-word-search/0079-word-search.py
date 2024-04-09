58ccd6b9bfd61c6c344fe669b8e044ad50291f92
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-09 12:37:53 -0400

    Time: 4503 ms (31.50%), Space: 16.6 MB (71.06%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,29 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
+        wordIndex = 0
         visited = set()
         
-        wordIndex = 0
-        
-        def wordSearch(wordIndex: int, row: int, col: int) -> bool:
+        def backtrack(wordIndex: int = 0, row: int = 0, col: int = 0) -> bool:
             if wordIndex >= len(word):
                 return True
             
-            if row < 0 or row >= ROWS or col < 0 or col >= COLS or word[wordIndex] != board[row][col] or (row, col) in visited:
+            if row < 0 or row >= ROWS or col < 0 or col >= COLS or board[row][col] != word[wordIndex] or (row, col) in visited:
                 return False
             
             visited.add((row, col))
             for rowOffset, colOffset in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 newRow, newCol = row + rowOffset, col + colOffset
-                if wordSearch(wordIndex + 1, newRow, newCol):
+                if backtrack(wordIndex + 1, newRow, newCol):
                     return True
             visited.remove((row, col))
             return False
         
         for row in range(ROWS):
             for col in range(COLS):
-                if wordSearch(0, row, col):
+                if backtrack(0, row, col):
                     return True
         
         return False
-            
