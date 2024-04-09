76875905ae57f93f59335b209a54af83e6ba8de0
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-08 22:42:26 -0400

    Time: 1023 ms (20.23%), Space: 310.5 MB (6.45%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -6,8 +6,12 @@ class Solution:
         def dp(index1: int = 0, index2: int = 0) -> int:
             if index1 == M or index2 == N:
                 return 0
+            
+            # if the first character of each string is the same
             if text1[index1] == text2[index2]:
                 return 1 + dp(index1 + 1, index2 + 1)
+            
+            # not the same, so one or both chracters will not be used
             return max(dp(index1 + 1, index2), dp(index1, index2 + 1))
         
         return dp()
