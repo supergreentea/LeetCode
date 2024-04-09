23b85aadf947cf9d19011e37781745a1911c6b4d
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-09 01:24:30 -0400

    Time: 30 ms (97.95%), Space: 17.4 MB (20.30%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,21 +1,20 @@
+from functools import cache
+
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s3) != len(s1) + len(s2):
-            return False
         
         @cache
-        def dp(s1_index: int = 0, s2_index: int = 0) -> bool:
-            s3_index = s1_index + s2_index
-            if s3_index == len(s3):
+        def dp(index1: int = 0, index2: int = 0) -> bool:
+            if index1 == len(s1) and index2 == len(s2) and index1 + index2 == len(s3):
                 return True
-            for i in range(s1_index, len(s1)):
-                if s1[s1_index: i + 1] == s3[s3_index: s3_index + (i - s1_index) + 1]:
-                    if dp(i + 1, s2_index):
-                        return True
-            for i in range(s2_index, len(s2)):
-                if s2[s2_index: i + 1] == s3[s3_index: s3_index + (i - s2_index) + 1]:
-                    if dp(s1_index, i + 1):
-                        return True
+            index3 = index1 + index2
+            if index1 < len(s1) and s1[index1] == s3[index3] and dp(index1 + 1, index2):
+                return True
+            if index2 < len(s2) and s2[index2] == s3[index3] and dp(index1, index2 + 1):
+                return True
+            return False
+        
+        if len(s1) + len(s2) != len(s3):
             return False
         
         return dp()
