654d39c8e19d303b5b8f7f6621df4cdec14f0308
supergreentea <45723762+supergreentea@users.noreply.github.com>
2024-04-09 22:11:51 -0400

    Time: 179 ms (71.61%), Space: 19.2 MB (85.32%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,9 +3,11 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        ROWS, COLS = len(rooms), len(rooms[0])
         INF = 2147483647
         queue = deque()
+        ROWS, COLS = len(rooms), len(rooms[0])
+        
+        # add the gates to the initial queue
         for row in range(ROWS):
             for col in range(COLS):
                 if rooms[row][col] == 0:
@@ -13,12 +15,8 @@ class Solution:
         
         while queue:
             row, col = queue.popleft()
-            for rowOffset, colOffset in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
+            for rowOffset, colOffset in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 newRow, newCol = row + rowOffset, col + colOffset
-                if newRow >= 0 and newRow < ROWS and newCol >= 0 and newCol < COLS \
-                    and rooms[newRow][newCol] == INF:
+                if newRow >= 0 and newRow < ROWS and newCol >= 0 and newCol < COLS and rooms[newRow][newCol] == INF:
                     rooms[newRow][newCol] = rooms[row][col] + 1
                     queue.append((newRow, newCol))
-        
-        
-        
